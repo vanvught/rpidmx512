310e6bb9fbda11f4946791ec8e27327ab7328574
Arjan <Arjan.van.Vught@gmail.com>
2014-02-15 15:27:24 +0100

    License statement added

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ OpenDMX
 =======
 
 DMX controller for Raspberry Pi
+
+Linux
+
+\bob
+\bw_i2cdetect
+\bw_spi_dio-tester-libbob
+\bw_spi_lcd-tester-libbob
+\mcp23s08-tester-libbob
+
+Baremetal
+
+\emmc
+\fb
+\ff9b
+\firmware-template
+\hal
+\pl011_dmx512
+\pl011_uart
+\sdcard-emmc-libff9b
--- bob/include/bw_lcd.h
+++ bob/include/bw_lcd.h
@@ -1,3 +1,24 @@
+/* Copyright (C) 2014 by Arjan van Vught <pm @ http://www.raspberrypi.org/forum/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #ifndef BW_LCD_H_
 #define BW_LCD_H_
 
@@ -47,10 +68,10 @@
 #define lcd_set_contrast				bw_spi_lcd_set_contrast
 #define lcd_set_backlight				bw_spi_lcd_set_backlight
 #define lcd_set_backlight_temp			bw_spi_lcd_set_backlight_temp
-#define lcd__startup_message_line_1		bw_spi_lcd__startup_message_line_1
-#define lcd__startup_message_line_2		bw_spi_lcd__startup_message_line_2
-#define lcd__startup_message_line_3		bw_spi_lcd__startup_message_line_3
-#define lcd__startup_message_line_4		bw_spi_lcd__startup_message_line_4
+#define lcd_startup_message_line_1		bw_spi_lcd_startup_message_line_1
+#define lcd_startup_message_line_2		bw_spi_lcd_startup_message_line_2
+#define lcd_startup_message_line_3		bw_spi_lcd_startup_message_line_3
+#define lcd_startup_message_line_4		bw_spi_lcd_startup_message_line_4
 #define lcd_read_id						bw_spi_lcd_read_id
 #endif
 
