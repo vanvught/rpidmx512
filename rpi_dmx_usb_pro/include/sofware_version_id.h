2d2ceb942a686ff54ebe495d4b29ffabd67c423c
Arjan van Vught <Arjan.van.Vught@gmail.com>
2015-07-11 19:27:11 +0200

    Latest build

--- rpi_dmx_usb_pro/include/sofware_version_id.h
+++ rpi_dmx_usb_pro/include/sofware_version_id.h
@@ -1,2 +1,2 @@
-// Generated Sat Jul 11 16:22:01 CEST 2015
-static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1436624522;
+// Generated Sat Jul 11 16:40:10 CEST 2015
+static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1436625610;
