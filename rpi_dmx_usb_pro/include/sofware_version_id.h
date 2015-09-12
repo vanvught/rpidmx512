08849db6e5bd2510db6e975af8a4e6f876d5a89a
Arjan van Vught <Arjan.van.Vught@gmail.com>
2015-09-12 12:49:09 +0200

    Latest stable build

--- rpi_dmx_usb_pro/include/sofware_version_id.h
+++ rpi_dmx_usb_pro/include/sofware_version_id.h
@@ -1,2 +1,2 @@
-// Generated Wed Sep 9 17:28:22 CEST 2015
-static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1441812502;
+// Generated Sat Sep 12 12:40:21 CEST 2015
+static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1442054422;
--- rpi_rdm_responder/include/sofware_version_id.h
+++ rpi_rdm_responder/include/sofware_version_id.h
@@ -1,2 +1,2 @@
-// Generated Wed Sep 9 17:28:27 CEST 2015
-static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1441812507;
+// Generated Sat Sep 12 12:40:27 CEST 2015
+static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1442054427;
