82aee971ebf0c60188e6f04c3c8af02c46f4aec1
Arjan van Vught <Arjan.van.Vught@gmail.com>
2016-01-31 15:37:39 +0100

    Latest stable build (second release) : Pi Zero support

--- rpi_dmx_usb_pro/include/sofware_version_id.h
+++ rpi_dmx_usb_pro/include/sofware_version_id.h
@@ -1,2 +1,2 @@
-// Generated Sun Sep 13 13:00:59 CEST 2015
-static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1442142060;
+// Generated Sun Jan 31 10:53:46 CET 2016
+static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1454234026;
