efc783ed125b2cce4ead3cd970b0179b5624b5d7
Arjan van Vught <Arjan.van.Vught@gmail.com>
2016-02-02 20:01:45 +0100

    RPi 2 : Check for HYP mode, switch back to SVC mode
    The firmware changed back in October 2015 put the RPi 2 into HYP mode on
    boot.

--- rpi_dmx_analyzer-slave/firmware/vectors.s
+++ rpi_dmx_analyzer-slave/firmware/vectors.s
@@ -41,6 +41,27 @@ irq_handler:		.word irq
 fiq_handler:		.word fiq
 
 reset:
+#if defined ( RPI2 )
+	@ Return current CPU ID (0..3)
+	mrc p15, 0, r0, c0, c0, 5 			@ r0 = Multiprocessor Affinity Register (MPIDR)
+	ands r0, #3							@ r0 = CPU ID (Bits 0..1)
+	bne hang 							@ If (CPU ID != 0) Branch To Infinite Loop (Core ID 1..3)
+	cpsid if							@ Disable IRQ & FIQ
+    @ Check for HYP mode
+	mrs	r0 , cpsr
+	eor	r0, r0, #0x1A
+	tst	r0, #0x1F
+	bic	r0 , r0 , #0x1F					@ clear mode bits
+	orr	r0 , r0 , #MODE_SVC|I_BIT|F_BIT	@ mask IRQ/FIQ bits and set SVC mode
+	bne	2f								@ branch if not HYP mode
+	orr	r0, r0, #0x100					@ mask Abort bit
+	adr	lr, 3f
+	msr	spsr_cxsf, r0
+	.word	0xE12EF30E					@ msr ELR_hyp, lr
+	.word	0xE160006E					@ eret
+2:	msr cpsr_c, r0
+3:
+#endif
     @ Copy vectors (including fiq handler) to 0x0000
     ldr   r1, =_start
     mov   r2, #0x0000
