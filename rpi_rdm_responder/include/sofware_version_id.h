b73841044ee62ab3e78dd753e6d07fcc788df20f
Arjan van Vught <Arjan.van.Vught@gmail.com>
2016-02-08 18:07:27 +0100

    Latest stable build

--- rpi_rdm_responder/include/sofware_version_id.h
+++ rpi_rdm_responder/include/sofware_version_id.h
@@ -1,2 +1,2 @@
-// Generated Sun Feb 7 20:45:38 CET 2016
-static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1454874338;
+// Generated Mon Feb 8 17:37:44 CET 2016
+static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1454949464;
