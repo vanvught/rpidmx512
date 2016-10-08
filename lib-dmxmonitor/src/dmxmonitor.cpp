07959bbcb9ad7e4c0fb632b14ab2746d1518e4ae
Arjan van Vught <Arjan.van.Vught@gmail.com>
2016-10-08 21:13:56 +0200

    Minor bug fixed

--- lib-dmxmonitor/src/dmxmonitor.cpp
+++ lib-dmxmonitor/src/dmxmonitor.cpp
@@ -35,9 +35,6 @@
  *
  */
 DMXMonitor::DMXMonitor(void) {
-	for (int i = TOP_ROW; i < (TOP_ROW + 33); i++) {
-		console_clear_line(i);
-	}
 
 }
 
@@ -52,6 +49,10 @@ DMXMonitor::~DMXMonitor(void) {
  *
  */
 void DMXMonitor::Start(void) {
+	for (int i = TOP_ROW; i < (TOP_ROW + 33); i++) {
+		console_clear_line(i);
+	}
+
 	console_set_cursor(0, TOP_ROW);
 	console_puts("    00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31\n");
 
