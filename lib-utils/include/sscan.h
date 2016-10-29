c5c8394c0af2ed954972b7bc81b0d047873dba45
Arjan van Vught <Arjan.van.Vught@gmail.com>
2016-10-29 14:41:23 +0200

    Revert "Added function scan_uuid"
    This reverts commit b49d704597f3b353be2639fcea4caf174f4ee637.

--- lib-utils/include/sscan.h
+++ lib-utils/include/sscan.h
@@ -37,7 +37,6 @@ extern int sscan_uint16_t(const char *, const char *, /*@out@*/uint16_t *);
 extern int sscan_uint32_t(const char *, const char *, /*@out@*/uint32_t *);
 extern int sscan_char_p(const char *, const char *, /*@out@*/char *, /*@out@*/uint8_t *);
 extern int sscan_ip_address(const char *, const char *, /*@out@*/uint32_t *);
-extern int sscan_uuid(const char *, const char *, /*@out@*/char *, /*@out@*/uint8_t *);
 
 #ifdef __cplusplus
 }
