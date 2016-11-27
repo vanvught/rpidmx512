51c433e1c87238084b5d92164bf239ff42aacc55
Arjan van Vught <Arjan.van.Vught@gmail.com>
2016-11-27 18:10:10 +0100

    V1.1
    Added real-time DMX statistics.
    Added support Raspberry Pi 2 v1.2
    The kernel7.img is running on both Raspberry Pi 2 and 3 now.

--- rpi_dmx_monitor/firmware/main.cpp
+++ rpi_dmx_monitor/firmware/main.cpp
@@ -29,21 +29,37 @@
 
 #include "dmxmonitor.h"
 #include "hardware.h"
+#include "console.h"
 #include "led.h"
 #include "dmx.h"
+
 #include "software_version.h"
 
 extern "C" {
 
-extern void dmx_monitor_init(void);
-extern void dmx_monitor(void);
+void __attribute__((interrupt("FIQ"))) c_fiq_handler(void) {}
+void __attribute__((interrupt("IRQ"))) c_irq_handler(void) {}
+
+#if !defined(UINT32_MAX)
+  #define UINT32_MAX  ((uint32_t)-1)
+#endif
 
 void notmain(void) {
+	uint32_t micros_previous = 0;
+
+	uint32_t updates_per_seconde_min = UINT32_MAX;
+	uint32_t updates_per_seconde_max = (uint32_t)0;
+	uint32_t slots_in_packet_min = UINT32_MAX;
+	uint32_t slots_in_packet_max = (uint32_t)0;
+	uint32_t slot_to_slot_min = UINT32_MAX;
+	uint32_t slot_to_slot_max = (uint32_t)0;
+	uint32_t break_to_break_min = UINT32_MAX;
+	uint32_t break_to_break_max = (uint32_t)0;
 
 	hardware_init();
 
-	printf("%s Compiled on %s at %s\n", hardware_get_board_model(), __DATE__, __TIME__);
-	printf("DMX Real-time Monitor [V%s]", SOFTWARE_VERSION);
+	printf("[V%s] %s Compiled on %s at %s\n", SOFTWARE_VERSION, hardware_board_get_model(), __DATE__, __TIME__);
+	printf("DMX Real-time Monitor");
 
 	dmx_init();
 	dmx_set_port_direction(DMX_PORT_DIRECTION_INP, true);
@@ -51,6 +67,12 @@ void notmain(void) {
 	DMXMonitor dmx_monitor;
 
 	dmx_monitor.Start();
+
+	console_set_cursor(0, 22);
+	console_puts("DMX updates/sec\n");
+	console_puts("Slots in packet\n");
+	console_puts("Slot to slot\n");
+	console_puts("Break to break");
 
 	hardware_watchdog_init();
 
@@ -66,6 +88,44 @@ void notmain(void) {
 				const uint16_t length = (uint16_t) (dmx_statistics->statistics.slots_in_packet);
 				dmx_monitor.SetData(0, ++p, length); // Skip DMX START CODE
 			}
+		}
+
+		const uint32_t micros_now = hardware_micros();
+
+		if (micros_now - micros_previous > (uint32_t) (1E6 / 2)) {
+			const uint8_t *dmx_data = dmx_get_current_data();
+			const struct _dmx_data *dmx_statistics = (struct _dmx_data *)dmx_data;
+			const uint32_t dmx_updates_per_seconde = dmx_get_updates_per_seconde();
+
+			if (dmx_updates_per_seconde == 0) {
+				console_set_cursor(20, 22);
+				console_puts("---");
+				console_set_cursor(20, 23);
+				console_puts("---");
+				console_set_cursor(20, 24);
+				console_puts("---");
+				console_set_cursor(17, 25);
+				console_puts("-------");
+			} else {
+				updates_per_seconde_min = MIN(dmx_updates_per_seconde, updates_per_seconde_min);
+				updates_per_seconde_max = MAX(dmx_updates_per_seconde, updates_per_seconde_max);
+				slots_in_packet_min = MIN(dmx_statistics->statistics.slots_in_packet, slots_in_packet_min);
+				slots_in_packet_max = MAX(dmx_statistics->statistics.slots_in_packet, slots_in_packet_max);
+				slot_to_slot_min = MIN(dmx_statistics->statistics.slot_to_slot, slot_to_slot_min);
+				slot_to_slot_max = MAX(dmx_statistics->statistics.slot_to_slot, slot_to_slot_max);
+				break_to_break_min = MIN(dmx_statistics->statistics.break_to_break, break_to_break_min);
+				break_to_break_max = MAX(dmx_statistics->statistics.break_to_break, break_to_break_max);
+				console_set_cursor(20, 22);
+				printf("%3d     %3d / %d", (int)dmx_updates_per_seconde, (int)updates_per_seconde_min , (int)updates_per_seconde_max);
+				console_set_cursor(20, 23);
+				printf("%3d     %3d / %d", (int)dmx_statistics->statistics.slots_in_packet, (int)slots_in_packet_min, (int)slots_in_packet_max);
+				console_set_cursor(20, 24);
+				printf("%3d     %3d / %d", (int)dmx_statistics->statistics.slot_to_slot, (int)slot_to_slot_min, (int)slot_to_slot_max);
+				console_set_cursor(17, 25);
+				printf("%6d  %6d / %d", (int)dmx_statistics->statistics.break_to_break, (int)break_to_break_min, (int)break_to_break_max);
+			}
+
+			micros_previous = micros_now;
 		}
 
 		led_blink();
