9e21a161c9a8dd56e475bb19dc8ffb38913c0bcc
Arjan van Vught <Arjan.van.Vught@gmail.com>
2017-01-01 20:32:41 +0100

    Added support for LCD TC1602 with I2C I/O Expander PCF8574T

---
+++ lib-bob/include/tc1602.h
@@ -0,0 +1,70 @@
+/**
+ * @file tc1602.h
+ *
+ */
+/* Copyright (C) 2016 by Arjan van Vught mailto:info@raspberrypi-dmx.nl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef TC1602_H_
+#define TC1602_H_
+
+#define TC1602_RS 			(1<<0)	///< 0b00000001 # Register select bit
+#define TC1602_RW 			(1<<1)	///< 0b00000010 # Read/Write bit
+#define TC1602_EN			(1<<2)	///< 0b00000100 # Enable bit
+#define TC1602_BACKLIGHT	(1<<3)	///< 0b00001000 # Backlight bit
+	#define TC1602_NOBACKLIGHT	0x00
+
+#define TC1602_LINE_1		0x80	///< LCD RAM address for the 1st line
+#define TC1602_LINE_2		0xC0	///< LCD RAM address for the 2nd line
+
+#define EXEC_TIME_CMD		37		///< 37us
+#define EXEC_TIME_REG		43		///< 43us
+#define EXEC_TIME_CLS		1520	///< 1.52ms
+
+/// Instruction Code
+/// http://www.oppod.com/upload/download/20111224105943_57765.pdf
+/// 1. Clear Display
+#define TC1602_IC_CLS			(1<<0)	///< Clear all the display data by writing "20H" (space code) to all DDRAM address.
+/// 2. Return Home
+#define TC1602_IC_RETURNHOME	(1<<1)	///< Return cursor to its original site and return display to its original status, if shifted. Contents of DDRAM do not change.
+/// 3. Entry Mode Set
+#define TC1602_IC_ENTRY_MODE	(1<<2)	///< Set the moving direction of cursor and display.
+	#define TC1602_IC_ENTRY_MODE_SH		(1<<0)	///< Shift of entire display
+	#define TC1602_IC_ENTRY_MODE_DEC		0	///< cursor/blink moves to left and DDRAM address is decreased by 1.
+	#define TC1602_IC_ENTRY_MODE_INC	(1<<1)	///< cursor/blink moves to right and DDRAM address is increased by 1.
+/// 4. Display ON/OFF Control
+#define TC1602_IC_DISPLAY		(1<<3)	///< Set display(D), cursor(C), and blinking of cursor(B) on/off control bit
+ 	#define TC1602_IC_DISPLAY_BLINK_OFF		0	///< Cursor blink is off.
+	#define TC1602_IC_DISPLAY_BLINK_ON	(1<<0)	///< Cursor blink is on, that performs alternate between all the high data and display character at the cursor position.
+	#define TC1602_IC_DISPLAY_CURSOR_OFF	0	///< Cursor is disappeared in current display, but I/D register remains its data.
+	#define TC1602_IC_DISPLAY_CURSOR_ON	(1<<1)	///< Cursor is turned on.
+ 	#define TC1602_IC_DISPLAY_OFF			0	///< The display is turned off, but display data is remained in DDRAM.
+	#define TC1602_IC_DISPLAY_ON		(1<<2)	///< The entire display is turned on.
+/// 6. Function Set
+#define TC1602_IC_FUNC			(1<<5)	///< Set interface data length, numbers of display lines, display font type
+	#define TC1602_IC_FUNC_4BIT			0	///< 4-bit bus mode with MPU.
+	#define TC1602_IC_FUNC_8BIT		(1<<4)	///< 8-bit bus mode with MPU.
+	#define TC1602_IC_FUNC_1LINE		0	///< 1-line display mode.
+	#define TC1602_IC_FUNC_2LINE	(1<<3)	///< 2-line display mode is set.
+	#define TC1602_IC_FUNC_5x8DOTS		0	///< 5 x 8 dots format display mode.
+	#define TC1602_IC_FUNC_5x11DOTS	(1<<2)	///< 5 x11 dots format display mode.
+
+#endif /* TC1602_H_ */
---
+++ lib-bob/src/tc1602_i2c.c
@@ -0,0 +1,186 @@
+/**
+ * @file tc1602_i2c.c
+ *
+ */
+/* Copyright (C) 2016 by Arjan van Vught mailto:info@raspberrypi-dmx.nl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+#include "bcm2835.h"
+
+#ifdef BARE_METAL
+#include "bcm2835_i2c.h"
+#endif
+
+#include "tc1602.h"
+#include "tc1602_i2c.h"
+
+#include "device_info.h"
+
+/**
+ *
+ * @param data
+ */
+static void i2c_write(const uint8_t data) {
+	BCM2835_BSC1->C = BCM2835_BSC_C_CLEAR_1;
+	BCM2835_BSC1->S = (uint32_t) (BCM2835_BSC_S_CLKT | BCM2835_BSC_S_ERR | BCM2835_BSC_S_DONE);
+	BCM2835_BSC1->DLEN = (uint32_t) 1;
+	BCM2835_BSC1->FIFO = (uint32_t) data;
+	BCM2835_BSC1->C = (uint32_t) (BCM2835_BSC_C_I2CEN | BCM2835_BSC_C_ST);
+	while (!(BCM2835_BSC1->S & BCM2835_BSC_S_DONE)) {}
+	BCM2835_BSC1->S = BCM2835_BSC_S_DONE;
+}
+
+/**
+ *
+ * @param device_info
+ */
+static void lcd_i2c_setup(const device_info_t *device_info) {
+	bcm2835_i2c_setSlaveAddress(device_info->slave_address);
+	bcm2835_i2c_setClockDivider(BCM2835_I2C_CLOCK_DIVIDER_626);
+}
+
+/**
+ *
+ * @param device_info
+ * @param data
+ */
+static void lcd_toggle_enable(const device_info_t *device_info, const uint8_t data) {
+	i2c_write(data | TC1602_EN | TC1602_BACKLIGHT);
+	i2c_write((data & ~TC1602_EN) | TC1602_BACKLIGHT);
+}
+
+/**
+ *
+ * @param device_info
+ * @param data
+ */
+static void write_4bits(const device_info_t *device_info, const uint8_t data) {
+	lcd_i2c_setup(device_info);
+
+	i2c_write(data);
+	lcd_toggle_enable(device_info, data);
+}
+
+/**
+ *
+ * @param device_info
+ * @param data
+ */
+static void write_cmd(const device_info_t *device_info, const uint8_t cmd) {
+	write_4bits(device_info, (cmd & 0xF0));
+	write_4bits(device_info, ((cmd << 4) & 0xF0));
+	udelay(EXEC_TIME_CMD);
+}
+
+/**
+ *
+ * @param device_info
+ * @param data
+ */
+static void write_reg(const device_info_t *device_info, const uint8_t reg) {
+	write_4bits(device_info, TC1602_RS | (reg & 0xF0));
+	write_4bits(device_info, TC1602_RS | ((reg << 4) & 0xF0));
+	udelay(EXEC_TIME_REG);
+}
+
+/**
+ *
+ * @param device_info
+ * @return
+ */
+uint8_t tc1602_i2c_start(device_info_t *device_info) {
+#if !defined(BARE_METAL)
+	if bcm2835_init() != 1) {
+		return 1;
+	}
+#endif
+	bcm2835_i2c_begin();
+
+	if (device_info->slave_address == (uint8_t) 0) {
+		device_info->slave_address = TC1602_I2C_DEFAULT_SLAVE_ADDRESS;
+	}
+
+	write_cmd(device_info, 0x33);	///< 110011 Initialize
+	write_cmd(device_info, 0x32);	///< 110010 Initialize
+
+	write_cmd(device_info, TC1602_IC_FUNC | TC1602_IC_FUNC_4BIT | TC1602_IC_FUNC_2LINE | TC1602_IC_FUNC_5x8DOTS); ///< Data length, number of lines, font size
+	write_cmd(device_info, TC1602_IC_DISPLAY | TC1602_IC_DISPLAY_ON | TC1602_IC_DISPLAY_CURSOR_OFF | TC1602_IC_DISPLAY_BLINK_OFF);	///< Display On,Cursor Off, Blink Off
+	write_cmd(device_info, TC1602_IC_CLS);
+	udelay(EXEC_TIME_CLS - EXEC_TIME_CMD);
+	write_cmd(device_info, TC1602_IC_ENTRY_MODE | TC1602_IC_ENTRY_MODE_INC);	///< Cursor move direction
+
+	return 0;
+}
+
+/**
+ *
+ * @param device_info
+ * @param data
+ * @param length
+ */
+void tc1602_i2c_text(const device_info_t *device_info, const char *data, uint8_t length) {
+	uint8_t i;
+
+	if (length > 16) {
+		length = 16;
+	}
+
+	for (i = 0; i < length; i++) {
+		write_reg(device_info, data[i]);
+	}
+
+}
+
+/**
+ *
+ * @param device_info
+ * @param data
+ * @param length
+ */
+void tc1602_i2c_text_line_1(const device_info_t *device_info, const char *data, const uint8_t length) {
+	write_cmd(device_info, TC1602_LINE_1);
+	tc1602_i2c_text(device_info, data, length);
+}
+
+/**
+ *
+ * @param device_info
+ * @param data
+ * @param length
+ */
+void tc1602_i2c_text_line_2(const device_info_t *device_info, const char *data, const uint8_t length) {
+	write_cmd(device_info, TC1602_LINE_2);
+	tc1602_i2c_text(device_info, data, length);
+}
+
+/**
+ *
+ * @param device_info
+ */
+void tc1602_i2c_cls(const device_info_t *device_info) {
+	write_cmd(device_info, TC1602_IC_CLS);
+	udelay(EXEC_TIME_CLS - EXEC_TIME_CMD);
+}
+
+
+
+
