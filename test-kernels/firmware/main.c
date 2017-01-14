0d2831d255d306090d0f00fddac913f07c86f062
Arjan van Vught <Arjan.van.Vught@gmail.com>
2017-01-14 16:58:58 +0100

    V1.0
    To use for retrieving the Raspberry Pi model identifier and other
    information.
    To use for testing the communication with the ESP8266.
    It detects several I2C LCD 16x2 devices.

--- test-kernels/firmware/main.c
+++ test-kernels/firmware/main.c
@@ -2,7 +2,7 @@
  * @file main.c
  *
  */
-/* Copyright (C) 2016 by Arjan van Vught mailto:info@raspberrypi-dmx.nl
+/* Copyright (C) 2016, 2017 by Arjan van Vught mailto:info@raspberrypi-dmx.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,18 +26,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#include "bcm2835_vc.h"
 #include "arm/arm.h"
+#include "bcm2835_vc.h"
 
 #include "hardware.h"
 #include "irq_timer.h"
 #include "console.h"
 #include "wifi.h"
+#include "lcd.h"
+
+#include "software_version.h"
 
 void __attribute__((interrupt("FIQ"))) c_fiq_handler(void) {}
 void __attribute__((interrupt("IRQ"))) c_irq_handler(void) {}
 
-static const char *gpfsel_names[] = { "IN  ", "OUT ", "ALT5", "ALT4", "ALT0", "ALT1", "ALT2", "ALT3" };
+static const char *gpfsel_names[] = { "IN", "OUT ", "ALT5", "ALT4", "ALT0", "ALT1", "ALT2", "ALT3" };
 
 static volatile unsigned led_state = 0;
 
@@ -49,14 +52,6 @@ void led_handler(const uint32_t clo) {
 	hardware_led_set(state);
 
 	BCM2835_ST->C3 = clo + (uint32_t) 1000000;
-}
-
-void time_out_handler(const uint32_t clo) {
-	BCM2835_ST->C3 = clo;
-	console_save_color();
-	console_set_fg_color(CONSOLE_RED);
-	console_puts("\nFailed connecting to ESP8266\n");
-	console_restore_color();
 }
 
 void handle_rc(int rc) {
@@ -74,15 +69,15 @@ void handle_rc(int rc) {
 void notmain(void) {
 	hardware_init();
 
-	printf("%s [%x]\n\n", hardware_board_get_model(), (unsigned int)hardware_board_get_model_id());
+	printf("[V%s] %s [%x]\n\n", SOFTWARE_VERSION, hardware_board_get_model(), (unsigned int)hardware_board_get_model_id());
 #if defined (RPI1)
-	printf("kernel.img : ");
+	console_puts("kernel.img : ");
 #elif defined (RPI2)
-	printf("kernel7.img : ");
+	console_puts("kernel7.img : ");
 #elif defined (RPI3)
-	printf("kernel8.img : ");
+	console_puts("kernel8.img : ");
 #else
-	printf("Unknown kernel : ");
+	console_puts("Unknown kernel : ");
 #endif
 	printf("Compiled on %s at %s\n\n", __DATE__, __TIME__);
 
@@ -115,32 +110,55 @@ void notmain(void) {
 	for (n = 0; n < 6; n++) {								// Loop for GPFSEL0 to GPFSEL5
 		val = *((unsigned int *) BCM2835_GPIO_BASE + n) & (n < 5 ? 0x3fffffff : 0x00000fff);
 		printf("GPFSEL%d ", n);
+
 		for (i = 0; i < 10; i++) {							// Loop for the 9 bit-triplets in each GPFSELn register
 			int fsel_num = n * 10 + i;						// The logical FSEL number
 			int bit_pos = i * 3;							// The position of the least significant bit in the triplet
 			unsigned int triplet = (val >> bit_pos) & 0x7;	// Extract the bit- triplet
-			printf("%2d:%s ", fsel_num, gpfsel_names[triplet]);
-			if (n == 5 && i == 3)							// break out of loop after FSEL53 (last triplet)
+
+			if (triplet == 0) {
+				int in_value;
+				if (fsel_num <= 31) {
+					uint32_t value = BCM2835_GPIO->GPLEV0;
+					in_value = (value & (1 << fsel_num)) ? 1 : 0;
+				} else {
+					uint32_t value = BCM2835_GPIO->GPLEV1;
+					in_value = (value & (1 << (fsel_num - 32))) ? 1 : 0;
+				}
+				printf("%2d:%s:%d ", fsel_num, gpfsel_names[triplet], in_value);
+			} else {
+				printf("%2d:%s ", fsel_num, gpfsel_names[triplet]);
+			}
+
+			if (n == 5 && i == 3) {							// break out of loop after FSEL53 (last triplet)
 				break;
+			}
 		}
-		printf("\n");
+
+		console_puts("\n");
 	}
 
-	printf("\n");
+	console_puts("\n");
+
+	if (lcd_detect()) {
+		console_puts("I2C LCD 16x2 found\n");
+	}
 
 	irq_timer_init();
-	irq_timer_set(IRQ_TIMER_1, time_out_handler);
 	irq_timer_set(IRQ_TIMER_3, led_handler);
-	BCM2835_ST->C1 = BCM2835_ST->CLO + (uint32_t) 1000000;
 	BCM2835_ST->C3 = BCM2835_ST->CLO + (uint32_t) 1000000;
 
-	wifi_init("");
-
-	irq_timer_set(IRQ_TIMER_1, NULL);
-
-	printf("ESP8266 information\n");
-	printf(" SDK      : %s\n", system_get_sdk_version());
-	printf(" Firmware : %s\n\n", wifi_get_firmware_version());
+	if (wifi_detect()) {
+		wifi_init("");
+		console_puts("ESP8266 information\n");
+		printf(" SDK      : %s\n", system_get_sdk_version());
+		printf(" Firmware : %s\n\n", wifi_get_firmware_version());
+	} else {
+		console_save_color();
+		console_set_fg_color(CONSOLE_RED);
+		console_puts("\nFailed connecting to ESP8266\n");
+		console_restore_color();
+	}
 
 	while (1) {
 	}
