f8a58c3a696568f2aa22b6a1dfd198329506ccba
Arjan van Vught <Arjan.van.Vught@gmail.com>
2017-06-03 21:02:48 +0200

    V1.5
    Using updated wifi library (lib-esp8266)

--- rpi_wifi_e131_dmx/firmware/main.cpp
+++ rpi_wifi_e131_dmx/firmware/main.cpp
@@ -30,53 +30,39 @@
 #include "hardware_uuid.h"
 
 #include "led.h"
-#include "console.h"
 
-#include "wifi.h"
-#include "udp.h"
+#include "console.h"
+#include "oled.h"
+
 #include "inet.h"
 #include "uuid.h"
-
-#include "ap_params.h"
-#include "network_params.h"
-#include "fota_params.h"
-#include "fota.h"
 
 #include "e131.h"
 #include "e131bridge.h"
 #include "e131params.h"
+
 #include "dmxsend.h"
 #include "dmxparams.h"
 #include "dmxmonitor.h"
 
-#include "oled.h"
+#include "wifi.h"
+#include "wifi_udp.h"
 
 #include "software_version.h"
-
-#define OLED_CONNECTED(b,f)	\
-do {						\
-	if(b) {					\
-		f;					\
-	}						\
-} while (0);
 
 extern "C" {
 
 void notmain(void) {
 	_output_type output_type = OUTPUT_TYPE_MONITOR;
-	uint8_t mac_address[6] ALIGNED;
-	struct ip_info ip_config;
 	E131Params e131params;
 	DMXParams dmxparams;
 	uuid_t uuid;
 	char uuid_str[UUID_STRING_LENGTH + 1] ALIGNED;
-	oled_info_t oled_info;
-	bool oled_connected;
+	oled_info_t oled_info = { OLED_128x64_I2C_DEFAULT };
+	bool oled_connected = false;
+	struct ip_info ip_config;
 
 	hardware_init();
-
-	oled_info.slave_address = 0;
-	oled_info.type = OLED_PANEL_128x64;
 
 	oled_connected = oled_start(&oled_info);
 
@@ -107,89 +93,15 @@ void notmain(void) {
 
 	console_set_top_row(3);
 
-	(void) ap_params_init();
-	const char *ap_password = ap_params_get_password();
-
-	hardware_watchdog_init();
-
-	console_status(CONSOLE_YELLOW, "Starting Wifi ...");
-	OLED_CONNECTED(oled_connected, oled_status(&oled_info, "Starting Wifi ..."));
-
-	wifi_init(ap_password);
-
-	hardware_watchdog_stop();
-
-	printf("ESP8266 information\n");
-	printf(" SDK      : %s\n", system_get_sdk_version());
-	printf(" Firmware : %s\n\n", wifi_get_firmware_version());
-
-	if (network_params_init()) {
-		console_status(CONSOLE_YELLOW, "Changing to Station mode ...");
-		OLED_CONNECTED(oled_connected, oled_status(&oled_info, "Changing to Station mode ..."));
-
-		if (network_params_is_use_dhcp()) {
-			wifi_station(network_params_get_ssid(), network_params_get_password());
-		} else {
-			ip_config.ip.addr = network_params_get_ip_address();
-			ip_config.netmask.addr = network_params_get_net_mask();
-			ip_config.gw.addr = network_params_get_default_gateway();
-			wifi_station_ip(network_params_get_ssid(), network_params_get_password(), &ip_config);
-		}
-	}
-
-	const _wifi_mode opmode = wifi_get_opmode();
-
-	if (opmode == WIFI_STA) {
-		printf("WiFi mode : Station\n");
-	} else {
-		printf("WiFi mode : Access Point (authenticate mode : %s)\n", *ap_password == '\0' ? "Open" : "WPA_WPA2_PSK");
-	}
-
-	if (wifi_get_macaddr(mac_address)) {
-		printf(" MAC address : "MACSTR "\n", MAC2STR(mac_address));
-	} else {
-		console_error("wifi_get_macaddr");
-		OLED_CONNECTED(oled_connected, oled_status(&oled_info, "E: wifi_get_macaddr"));
-	}
-
-	printf(" Hostname    : %s\n", wifi_station_get_hostname());
-
-	if (wifi_get_ip_info(&ip_config)) {
-		printf(" IP-address  : " IPSTR "\n", IP2STR(ip_config.ip.addr));
-		printf(" Netmask     : " IPSTR "\n", IP2STR(ip_config.netmask.addr));
-		printf(" Gateway     : " IPSTR "\n", IP2STR(ip_config.gw.addr));
-		if (opmode == WIFI_STA) {
-			const _wifi_station_status status = wifi_station_get_connect_status();
-			printf("      Status : %s\n", wifi_station_status(status));
-			if (status != WIFI_STATION_GOT_IP){
-				printf("SSID : %s\n", network_params_get_ssid());
-				console_error("Not connected!");
-				if (oled_connected) {
-					oled_set_cursor(&oled_info, 2, 0);
-					oled_puts(&oled_info, wifi_station_status(status));
-					oled_set_cursor(&oled_info, 5, 0);
-					oled_printf(&oled_info, "SSID : %s\n", network_params_get_ssid());
-					oled_status(&oled_info, "<Not connected!>");
-				}
-				for(;;);
-			}
-		}
-	} else {
-		console_error("wifi_get_ip_info");
-		OLED_CONNECTED(oled_connected, oled_status(&oled_info, "E: wifi_get_ip_info"));
-	}
-
-	if (fota_params_init()) {
-		OLED_CONNECTED(oled_connected, oled_status(&oled_info, "FOTA mode"));
-		console_newline();
-		fota(fota_params_get_server());
-		for(;;);
+	if (!wifi(&ip_config)) {
+		for (;;)
+			;
 	}
 
 	console_status(CONSOLE_YELLOW, "Starting UDP ...");
 	OLED_CONNECTED(oled_connected, oled_status(&oled_info, "Starting UDP ..."));
 
-	udp_begin(E131_DEFAULT_PORT);
+	wifi_udp_begin(E131_DEFAULT_PORT);
 
 	console_status(CONSOLE_YELLOW, "Join group ...");
 	OLED_CONNECTED(oled_connected, oled_status(&oled_info, "Join group ..."));
@@ -198,7 +110,7 @@ void notmain(void) {
 	(void)inet_aton("239.255.0.0", &group_ip);
 	const uint16_t universe = e131params.GetUniverse();
 	group_ip = group_ip | ((uint32_t)(((uint32_t)universe & (uint32_t)0xFF) << 24)) | ((uint32_t)(((uint32_t)universe & (uint32_t)0xFF00) << 8));
-	udp_joingroup(group_ip);
+	wifi_udp_joingroup(group_ip);
 
 	E131Bridge bridge;
 	DMXSend dmx;
@@ -257,21 +169,21 @@ void notmain(void) {
 		}
 
 		oled_set_cursor(&oled_info, 1, 0);
-		if (opmode == WIFI_STA) {
-			oled_printf(&oled_info, "S: %s", network_params_get_ssid());
+		if (wifi_get_opmode() == WIFI_STA) {
+			(void) oled_printf(&oled_info, "S: %s", wifi_get_ssid());
 		} else {
-			oled_printf(&oled_info, "AP (%s)\n", *ap_password == '\0' ? "Open" : "WPA_WPA2_PSK");
+			(void) oled_printf(&oled_info, "AP (%s)\n", wifi_ap_is_open() ? "Open" : "WPA_WPA2_PSK");
 		}
 
 		oled_set_cursor(&oled_info, 2, 0);
-		oled_puts(&oled_info, "CID: ");
-		oled_puts(&oled_info, uuid_str);
+		(void) oled_puts(&oled_info, "CID: ");
+		(void) oled_puts(&oled_info, uuid_str);
 		oled_set_cursor(&oled_info, 4, 0);
-		oled_printf(&oled_info, "U: %d M: %s", bridge.getUniverse(), bridge.getMergeMode() == E131_MERGE_HTP ? "HTP" : "LTP");
+		(void) oled_printf(&oled_info, "U: %d M: %s", bridge.getUniverse(), bridge.getMergeMode() == E131_MERGE_HTP ? "HTP" : "LTP");
 		oled_set_cursor(&oled_info, 5, 0);
-		oled_printf(&oled_info, "M: " IPSTR "", IP2STR(group_ip));
+		(void) oled_printf(&oled_info, "M: " IPSTR "", IP2STR(group_ip));
 		oled_set_cursor(&oled_info, 6, 0);
-		oled_printf(&oled_info, "U: " IPSTR "", IP2STR(ip_config.ip.addr));
+		(void) oled_printf(&oled_info, "U: " IPSTR "", IP2STR(ip_config.ip.addr));
 	}
 
 	hardware_watchdog_init();
