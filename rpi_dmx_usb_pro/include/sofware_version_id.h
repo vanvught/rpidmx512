46bacf7365dc46f0e012c27465880ba88118ad7e
Arjan van Vught <Arjan.van.Vught@gmail.com>
2017-06-14 18:51:13 +0200

    V1.7
    Built with the latest libraries.

--- rpi_dmx_usb_pro/include/sofware_version_id.h
+++ rpi_dmx_usb_pro/include/sofware_version_id.h
@@ -1,2 +1,2 @@
-// Generated za 20 mei 2017 15:43:49 CEST
-static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1495287829;
+// Generated wo 14 jun 2017 18:44:21 CEST
+static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1497458661;
