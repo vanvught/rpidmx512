vanvught/rpidmx512

commit a7ecc2be1446c32d6ca74e87d5e5eb2d4fd86573

Code improvements
Preparing for removing linker option -lgcc

lib-hal/include/hardware.h
@@ -50,7 +50,7 @@ extern void hardware_init(void);
 extern void hardware_reboot(void);
 extern void hardware_led_init(void);
 extern void hardware_led_set(const int);
-extern const uint64_t hardware_uptime_seconds(void);
+extern const uint32_t hardware_uptime_seconds(void);
 extern const int32_t hardware_firmware_get_revision(void);
 extern /*@shared@*/const char *hardware_firmware_get_copyright(void);
 extern const uint8_t hardware_firmware_get_copyright_length(void);

lib-hal/src/hardware.c
@@ -53,8 +53,8 @@ static FATFS fat_fs;		/* File system object */
  *
  * @return The board uptime in seconds
  */
-const uint64_t hardware_uptime_seconds(void) {
-	return (((bcm2835_st_read() - hardware_init_startup_micros) / 1E6));
+const uint32_t hardware_uptime_seconds(void) {
+	return (uint32_t)(bcm2835_st_read() - hardware_init_startup_micros) / (uint32_t) 1000000;
 }
 
 /**

lib-hal/src/sys_time.c
@@ -96,12 +96,12 @@ void sys_time_set(const struct tm *tmbuf) {
  */
 time_t sys_time(time_t *__timer) {
 	dmb();
-	time_t elapsed = (time_t)((bcm2835_st_read() - sys_time_init_startup_micros) / 1E6);
+	time_t elapsed = (time_t) ((uint32_t) (bcm2835_st_read() - sys_time_init_startup_micros) / (uint32_t) 1000000);
 	dmb();
 
 	elapsed = elapsed + rtc_startup_seconds;
 
-	if (__timer != NULL ) {
+	if (__timer != NULL) {
 		*__timer = elapsed;
 	}
 
@@ -114,7 +114,7 @@ time_t sys_time(time_t *__timer) {
  */
 const uint32_t millis(void) {
 	dmb();
-	const uint32_t elapsed = (uint32_t)((bcm2835_st_read() - sys_time_init_startup_micros) / 1E3);
+	const uint32_t elapsed = ((uint32_t) (bcm2835_st_read() - sys_time_init_startup_micros) / (uint32_t) 1000);
 	dmb();
 
 	return elapsed;

lib-hal/src/led.c
@@ -30,7 +30,7 @@
 
 #include "hardware.h"
 
-static uint32_t ticks_per_second = (uint32_t) (1E6 / 2);	///< Blinking at 1Hz
+static uint32_t ticks_per_second = (uint32_t) (1000000 / 2);///< Blinking at 1Hz
 static uint32_t micros_previous = 0;						///<
 static uint32_t led_counter = 0;							///<
 

lib-lcd/.cproject
@@ -20,7 +20,7 @@
 							<option id="cdt.managedbuild.option.gnu.cross.prefix.1562319742" name="Prefix" superClass="cdt.managedbuild.option.gnu.cross.prefix"/>
 							<option id="cdt.managedbuild.option.gnu.cross.path.132626945" name="Path" superClass="cdt.managedbuild.option.gnu.cross.path"/>
 							<targetPlatform archList="all" binaryParser="org.eclipse.cdt.core.ELF" id="cdt.managedbuild.targetPlatform.gnu.cross.116250226" isAbstract="false" osList="all" superClass="cdt.managedbuild.targetPlatform.gnu.cross"/>
-							<builder id="cdt.managedbuild.builder.gnu.cross.548548032" keepEnvironmentInBuildfile="false" managedBuildOn="false" name="Gnu Make Builder" superClass="cdt.managedbuild.builder.gnu.cross"/>
+							<builder id="cdt.managedbuild.builder.gnu.cross.548548032" keepEnvironmentInBuildfile="false" managedBuildOn="false" name="Gnu Make Builder" parallelBuildOn="true" parallelizationNumber="optimal" superClass="cdt.managedbuild.builder.gnu.cross"/>
 							<tool id="cdt.managedbuild.tool.gnu.cross.c.compiler.1685510253" name="Cross GCC Compiler" superClass="cdt.managedbuild.tool.gnu.cross.c.compiler">
 								<option id="gnu.c.compiler.option.include.paths.358322002" name="Include paths (-I)" superClass="gnu.c.compiler.option.include.paths" valueType="includePath">
 									<listOptionValue builtIn="false" value="&quot;${workspace_loc:/lib-bob/include}&quot;"/>

commit a324b932874395f693b4a9b62a58c97db33b74df

Bug fixes
Bugs introduced with
https://github.com/vanvught/rpidmx512/commit/a7ecc2be1446c32d6ca74e87d5e5eb2d4fd86573

lib-hal/include/hardware.h
@@ -50,7 +50,7 @@ extern void hardware_init(void);
 extern void hardware_reboot(void);
 extern void hardware_led_init(void);
 extern void hardware_led_set(const int);
-extern const uint32_t hardware_uptime_seconds(void);
+extern const uint64_t hardware_uptime_seconds(void);
 extern const int32_t hardware_firmware_get_revision(void);
 extern /*@shared@*/const char *hardware_firmware_get_copyright(void);
 extern const uint8_t hardware_firmware_get_copyright_length(void);

lib-hal/src/hardware.c
@@ -28,7 +28,7 @@
 
 #include "arm/synchronize.h"
 
-#include "bcm2835.h"
+#include "bcm2835_st.h"
 #include "bcm2835_vc.h"
 #include "bcm2835_led.h"
 #include "bcm2835_rng.h"
@@ -40,49 +40,30 @@
 #include "smp.h"
 
 struct _hardware_led {
-	void (*init)(void);			///< Pointer to function for LED ACCT init (GPIO FSEL OUTPUT)
-	void (*set)(const int);		///< Pointer to function for LED ACCT on/off
+	void (*init)(void);		///< Pointer to function for LED ACCT init (GPIO FSEL OUTPUT)
+	void (*set)(const int);	///< Pointer to function for LED ACCT on/off
 }static _hardware_led_f = { led_init, led_set };
 
 static volatile uint64_t hardware_init_startup_micros = 0;	///<
 
 static FATFS fat_fs;		/* File system object */
 
-/**
- * @ingroup hal
- *
- * @return The board uptime in seconds
- */
-const uint32_t hardware_uptime_seconds(void) {
-	return (uint32_t)(bcm2835_st_read() - hardware_init_startup_micros) / (uint32_t) 1000000;
+const uint64_t hardware_uptime_seconds(void) {
+	return (uint64_t) (bcm2835_st_read() - hardware_init_startup_micros) / (uint64_t) 1000000;
 }
 
-/**
- * @ingroup hal
- *
- */
 void hardware_led_init(void) {
 	_hardware_led_f.init();
 }
 
-/**
- * @ingroup hal
- *
- * @param state
- */
 void hardware_led_set(const int state) {
 	_hardware_led_f.set(state);
 }
-
 
 const int32_t hardware_get_core_temperature(void) {
 	return bcm2835_vc_get_temperature() / 1000;
 }
 
-/**
- * @ingroup hal
- *
- */
 void hardware_init(void) {
 	int32_t board_revision;
 

lib-hal/src/sys_time.c
@@ -26,13 +26,13 @@
 #include <stdint.h>
 #include <time.h>
 
-#include "bcm2835.h"
+#include "bcm2835_st.h"
 #include "arm/synchronize.h"
 
 #include "mcp7941x.h"
 
 static volatile uint64_t sys_time_init_startup_micros = 0;	///<
-static volatile uint32_t rtc_startup_seconds = 0;			///<
+static volatile time_t rtc_startup_seconds = 0;				///<
 
 /**
  * @ingroup time
@@ -57,7 +57,7 @@ void sys_time_init(void) {
 		tmbuf.tm_isdst = 0; // 0 (DST not in effect, just take RTC time)
 		//tmbuf.tm_yday = 0;
 
-		rtc_startup_seconds = (uint32_t)mktime(&tmbuf);
+		rtc_startup_seconds = mktime(&tmbuf);
 		return;
 	}
 
@@ -72,7 +72,7 @@ void sys_time_init(void) {
 	tmbuf.tm_year = tm_rtc.tm_year;
 	tmbuf.tm_isdst = 0; // 0 (DST not in effect, just take RTC time)
 
-	rtc_startup_seconds = (uint32_t)mktime(&tmbuf);
+	rtc_startup_seconds = mktime(&tmbuf);
 }
 
 /**
@@ -82,7 +82,7 @@ void sys_time_init(void) {
  */
 void sys_time_set(const struct tm *tmbuf) {
 	sys_time_init_startup_micros = bcm2835_st_read();
-	rtc_startup_seconds = (uint32_t)mktime((struct tm *) tmbuf);
+	rtc_startup_seconds = mktime((struct tm *) tmbuf);
 }
 
 /**
@@ -96,7 +96,7 @@ void sys_time_set(const struct tm *tmbuf) {
  */
 time_t sys_time(time_t *__timer) {
 	dmb();
-	time_t elapsed = (time_t) ((uint32_t) (bcm2835_st_read() - sys_time_init_startup_micros) / (uint32_t) 1000000);
+	time_t elapsed = (time_t) ((bcm2835_st_read() - sys_time_init_startup_micros) / (uint64_t) 1000000);
 	dmb();
 
 	elapsed = elapsed + rtc_startup_seconds;