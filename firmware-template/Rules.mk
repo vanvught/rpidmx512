5d909a44a43e3a8de87a2d8cee55ad104553241c
Arjan van Vught <Arjan.van.Vught@gmail.com>
2017-08-06 20:23:47 +0200

    Major cleanup
    References to ARMv8 removed

--- firmware-template/Rules.mk
+++ firmware-template/Rules.mk
@@ -1,6 +1,12 @@
-ARMGNU ?= arm-none-eabi
+PREFIX ?= arm-none-eabi-
 
-LIBS += hal uuid lcd esp8266 bob i2c fb utils ff11 emmc bcm2835
+CC	= $(PREFIX)gcc
+CPP	= $(PREFIX)g++
+AS	= $(CC)
+LD	= $(PREFIX)ld
+AR	= $(PREFIX)ar
+
+LIBS += hal uuid lcd network esp8266 bob i2c fb properties utils ff11 emmc bcm2835
 
 DEFINES := $(addprefix -D,$(DEFINES))
 
@@ -17,8 +23,6 @@ LIB6  = $(addprefix -L../lib-,$(LIBS))
 LIB6 := $(addsuffix /lib, $(LIB6))
 LIB7  = $(addprefix -L../lib-,$(LIBS))
 LIB7 := $(addsuffix /lib7, $(LIB7))
-LIB8  = $(addprefix -L../lib-,$(LIBS))
-LIB8 := $(addsuffix /lib8, $(LIB8))
 
 # The variable for the ld -l flag 
 LDLIBS := $(addprefix -l,$(LIBS))
@@ -31,9 +35,6 @@ LIB6DEP := $(addsuffix .a, $(LIB6DEP))
 LIB7DEP = $(addsuffix /lib7/lib, $(LIBDEP))
 LIB7DEP := $(join $(LIB7DEP), $(LIBS))
 LIB7DEP := $(addsuffix .a, $(LIB7DEP))
-LIB8DEP = $(addsuffix /lib8/lib, $(LIBDEP))
-LIB8DEP := $(join $(LIB8DEP), $(LIBS))
-LIB8DEP := $(addsuffix .a, $(LIB8DEP))
 
 COPS_COMMON = -DBARE_METAL $(DEFINES) #-DNDEBUG
 COPS_COMMON += $(INCDIRS) $(LIBINCDIRS) $(addprefix -I,$(EXTRA_INCLUDES))
@@ -47,72 +48,52 @@ COPS7 = -mfpu=neon-vfpv4 -march=armv7-a -mtune=cortex-a7 #-fopt-info-vec-optimiz
 COPS7 += -DRPI2
 COPS7 += $(COPS_COMMON)
 
-COPS8 = -mfpu=neon-fp-armv8 -march=armv8-a -mtune=cortex-a53 #-fopt-info-vec-optimized
-COPS8 += -DRPI3
-COPS8 += $(COPS_COMMON)
-
 SOURCE = ./
 
 BUILD = build/
 BUILD7 = build7/
-BUILD8 = build8/
 
 C_OBJECTS = $(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.c,$(BUILD)$(sdir)/%.o,$(wildcard $(sdir)/*.c)))
 C_OBJECTS += $(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.cpp,$(BUILD)$(sdir)/%.o,$(wildcard $(sdir)/*.cpp)))
 C_OBJECTS7 = $(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.c,$(BUILD7)$(sdir)/%.o,$(wildcard $(sdir)/*.c)))
 C_OBJECTS7 += $(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.cpp,$(BUILD7)$(sdir)/%.o,$(wildcard $(sdir)/*.cpp)))
-C_OBJECTS8 = $(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.c,$(BUILD8)$(sdir)/%.o,$(wildcard $(sdir)/*.c)))
-C_OBJECTS8 += $(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.cpp,$(BUILD8)$(sdir)/%.o,$(wildcard $(sdir)/*.cpp)))
 
 BUILD_DIRS := $(addprefix build/,$(SRCDIR))
 BUILD7_DIRS := $(addprefix build7/,$(SRCDIR))
-BUILD8_DIRS := $(addprefix build8/,$(SRCDIR))
 
 OBJECTS := $(ASM_OBJECTS) $(C_OBJECTS)
 OBJECTS7 := $(ASM_OBJECTS7) $(C_OBJECTS7)
-OBJECTS8 := $(ASM_OBJECTS8) $(C_OBJECTS8)
 
 TARGET = kernel.img
 TARGET7 = kernel7.img
-TARGET8 = kernel8.img
 
 LIST = kernel.list
 LIST7 = kernel7.list
-LIST8 = kernel8.list
 
 MAP = kernel.map
 MAP7 = kernel7.map
-MAP8 = kernel8.map
 
 LINKER = firmware/memmap
 
 define compile-objects6
 $(BUILD)$1/%.o: $(SOURCE)$1/%.c
-	$(ARMGNU)-gcc $(COPS) -c $$< -o $$@
+	$(CC) $(COPS) -c $$< -o $$@
 	
 $(BUILD)$1/%.o: $(SOURCE)$1/%.cpp
-	$(ARMGNU)-g++ -pedantic -fno-exceptions -fno-unwind-tables -fno-rtti -std=c++11 $(COPS) -c $$< -o $$@	
+	$(CPP) -pedantic -fno-exceptions -fno-unwind-tables -fno-rtti -std=c++11 $(COPS) -c $$< -o $$@	
 endef
 
 define compile-objects7
 $(BUILD7)$1/%.o: $(SOURCE)$1/%.c
-	$(ARMGNU)-gcc $(COPS7) -c $$< -o $$@
+	$(CC) $(COPS7) -c $$< -o $$@
 	
 $(BUILD7)$1/%.o: $(SOURCE)$1/%.cpp
-	$(ARMGNU)-g++ -pedantic -fno-exceptions -fno-unwind-tables -fno-rtti -std=c++11 $(COPS7) -c $$< -o $$@		
-endef
-
-define compile-objects8
-$(BUILD8)$1/%.o: $(SOURCE)$1/%.c
-	$(ARMGNU)-gcc $(COPS8) -c $$< -o $$@
-	
-$(BUILD8)$1/%.o: $(SOURCE)$1/%.cpp
-	$(ARMGNU)-g++ -pedantic -fno-exceptions -fno-unwind-tables -fno-rtti -std=c++11 $(COPS8) -c $$< -o $$@		
+	$(CPP) -pedantic -fno-exceptions -fno-unwind-tables -fno-rtti -std=c++11 $(COPS7) -c $$< -o $$@		
 endef
 
 THISDIR = $(CURDIR)
 
-all : builddirs prerequisites $(TARGET) $(TARGET7) # $(TARGET8)
+all : builddirs prerequisites $(TARGET) $(TARGET7)
 	
 .PHONY: clean builddirs
 
@@ -120,52 +101,38 @@ buildlibs:
 	cd .. && ./makeall-lib.sh && cd $(THISDIR)
 
 builddirs:
-	@mkdir -p $(BUILD_DIRS) $(BUILD7_DIRS) # $(BUILD8_DIRS)
+	@mkdir -p $(BUILD_DIRS) $(BUILD7_DIRS)
 
 clean:
-	rm -rf $(BUILD) $(BUILD7) $(BUILD8)
-	rm -f $(TARGET) $(TARGET7) $(TARGET8)
-	rm -f $(MAP) $(MAP7) $(MAP8)
-	rm -f $(LIST) $(LIST7) $(LIST8)
+	rm -rf $(BUILD) $(BUILD7)
+	rm -f $(TARGET) $(TARGET7)
+	rm -f $(MAP) $(MAP7)
+	rm -f $(LIST) $(LIST7)
 
 # Build kernel.img
 
 $(BUILD)vectors.o : firmware/vectors.S
-	$(ARMGNU)-gcc $(COPS) -D__ASSEMBLY__ -c firmware/vectors.S -o $(BUILD)vectors.o
+	$(AS) $(COPS) -D__ASSEMBLY__ -c firmware/vectors.S -o $(BUILD)vectors.o
 	
 $(BUILD)main.elf : Makefile $(LINKER) $(BUILD)vectors.o $(OBJECTS) $(LIB6DEP)
-	$(ARMGNU)-ld $(BUILD)vectors.o $(OBJECTS) -Map $(MAP) -T $(LINKER) -o $(BUILD)main.elf $(LIB6) $(LDLIBS) #-lgcc
-	$(ARMGNU)-objdump -D $(BUILD)main.elf | $(ARMGNU)-c++filt > $(LIST)
+	$(LD) $(BUILD)vectors.o $(OBJECTS) -Map $(MAP) -T $(LINKER) -o $(BUILD)main.elf $(LIB6) $(LDLIBS) #-lgcc
+	$(PREFIX)objdump -D $(BUILD)main.elf | $(PREFIX)c++filt > $(LIST)
 
 $(TARGET) : $(BUILD)main.elf
-	$(ARMGNU)-objcopy $(BUILD)main.elf -O binary $(TARGET)
+	$(PREFIX)objcopy $(BUILD)main.elf -O binary $(TARGET)
 
 # Build kernel7.img
 
 $(BUILD7)vectors.o : firmware/vectors.S
-	$(ARMGNU)-gcc $(COPS7) -D__ASSEMBLY__ -c firmware/vectors.S -o $(BUILD7)vectors.o
+	$(AS) $(COPS7) -D__ASSEMBLY__ -c firmware/vectors.S -o $(BUILD7)vectors.o
 	
 $(BUILD7)main.elf : Makefile $(LINKER) $(BUILD7)vectors.o $(OBJECTS7) $(LIB7DEP)
-	$(ARMGNU)-ld $(BUILD7)vectors.o $(OBJECTS7) -Map $(MAP7) -T $(LINKER) -o $(BUILD7)main.elf $(LIB7) $(LDLIBS) #-lgcc
-	$(ARMGNU)-objdump -D $(BUILD7)main.elf | $(ARMGNU)-c++filt > $(LIST7)
+	$(LD) $(BUILD7)vectors.o $(OBJECTS7) -Map $(MAP7) -T $(LINKER) -o $(BUILD7)main.elf $(LIB7) $(LDLIBS) #-lgcc
+	$(PREFIX)objdump -D $(BUILD7)main.elf | $(PREFIX)c++filt > $(LIST7)
 
 $(TARGET7) : $(BUILD7)main.elf
-	$(ARMGNU)-objcopy $(BUILD7)main.elf -O binary $(TARGET7)
+	$(PREFIX)objcopy $(BUILD7)main.elf -O binary $(TARGET7)
 	
-# Build kernel8.img
-
-$(BUILD8)vectors.o : firmware/vectors.S
-	$(ARMGNU)-gcc $(COPS8) -D__ASSEMBLY__ -c firmware/vectors.S -o $(BUILD8)vectors.o
-	
-$(BUILD8)main.elf : Makefile $(LINKER) $(BUILD8)vectors.o $(OBJECTS8) $(LIB7DEP)
-	$(ARMGNU)-ld $(BUILD8)vectors.o $(OBJECTS8) -Map $(MAP8) -T $(LINKER) -o $(BUILD8)main.elf $(LIB8) $(LDLIBS) #-lgcc
-	$(ARMGNU)-objdump -D $(BUILD8)main.elf | $(ARMGNU)-c++filt > $(LIST8)
-
-$(TARGET8) : $(BUILD8)main.elf
-	$(ARMGNU)-objcopy $(BUILD8)main.elf -O binary $(TARGET8)	
-
 $(foreach bdir,$(SRCDIR),$(eval $(call compile-objects6,$(bdir))))
 	
 $(foreach bdir,$(SRCDIR),$(eval $(call compile-objects7,$(bdir))))
-
-$(foreach bdir,$(SRCDIR),$(eval $(call compile-objects8,$(bdir))))
--- firmware-template/lib/Rules.mk
+++ firmware-template/lib/Rules.mk
@@ -1,4 +1,10 @@
-ARMGNU ?= arm-none-eabi
+PREFIX ?= arm-none-eabi-
+
+CC	= $(PREFIX)gcc
+CPP	= $(PREFIX)g++
+AS	= $(CC)
+LD	= $(PREFIX)ld
+AR	= $(PREFIX)ar
 
 INCLUDES := -I./include
 INCLUDES += $(addprefix -I,$(EXTRA_INCLUDES))
@@ -17,10 +23,6 @@ COPS7 = -mfpu=neon-vfpv4 -march=armv7-a -mtune=cortex-a7 #-fopt-info-vec-optimiz
 COPS7 += -DRPI2
 COPS7 += $(COPS_COMMON)
 
-COPS8 = -mfpu=neon-fp-armv8 -march=armv8-a -mtune=cortex-a53 #-fopt-info-vec-optimized 
-COPS8 += -DRPI3
-COPS8 += $(COPS_COMMON)
-
 SOURCE = ./src
 
 CURR_DIR := $(notdir $(patsubst %/,%,$(CURDIR)))
@@ -28,75 +30,54 @@ LIB_NAME := $(patsubst lib-%,%,$(CURR_DIR))
 
 BUILD = build/
 BUILD7 = build7/
-BUILD8 = build8/
 
 OBJECTS := $(patsubst $(SOURCE)/%.c,$(BUILD)%.o,$(wildcard $(SOURCE)/*.c)) $(patsubst $(SOURCE)/%.cpp,$(BUILD)%.o,$(wildcard $(SOURCE)/*.cpp)) $(patsubst $(SOURCE)/%.S,$(BUILD)%.o,$(wildcard $(SOURCE)/*.S))
 OBJECTS7 := $(patsubst $(SOURCE)/%.c,$(BUILD7)%.o,$(wildcard $(SOURCE)/*.c)) $(patsubst $(SOURCE)/%.cpp,$(BUILD7)%.o,$(wildcard $(SOURCE)/*.cpp)) $(patsubst $(SOURCE)/%.S,$(BUILD7)%.o,$(wildcard $(SOURCE)/*.S))
-OBJECTS8 := $(patsubst $(SOURCE)/%.c,$(BUILD8)%.o,$(wildcard $(SOURCE)/*.c)) $(patsubst $(SOURCE)/%.cpp,$(BUILD8)%.o,$(wildcard $(SOURCE)/*.cpp)) $(patsubst $(SOURCE)/%.S,$(BUILD8)%.o,$(wildcard $(SOURCE)/*.S))
 
 TARGET = lib/lib$(LIB_NAME).a 
 TARGET7 = lib7/lib$(LIB_NAME).a
-TARGET8 = lib8/lib$(LIB_NAME).a
 
 LIST = lib.list
 LIST7 = lib7.list
-LIST8 = lib8.list
 
-all : builddirs $(TARGET) $(TARGET7) # $(TARGET8)
+all : builddirs $(TARGET) $(TARGET7)
 
 .PHONY: clean builddirs
 
 builddirs:
-	@mkdir -p build lib build7 lib7 # build8 lib8
+	@mkdir -p $(BUILD) lib $(BUILD7) lib7
 
 clean :
-	rm -rf $(BUILD) $(BUILD7) $(BUILD8)
-	rm -f $(TARGET) $(TARGET7) $(TARGET8)	
-	rm -f $(LIST) $(LIST7) $(LIST8)	
+	rm -rf $(BUILD) $(BUILD7)
+	rm -f $(TARGET) $(TARGET7)	
+	rm -f $(LIST) $(LIST7)	
 
 # ARM v6
 
 $(BUILD)%.o: $(SOURCE)/%.c
-	$(ARMGNU)-gcc $(COPS) $< -c -o $@
+	$(CC) $(COPS) $< -c -o $@
 	
 $(BUILD)%.o: $(SOURCE)/%.cpp
-	$(ARMGNU)-g++ $(COPS) -fno-rtti -std=c++11 $< -c -o $@		
+	$(CPP) $(COPS) -fno-rtti -std=c++11 $< -c -o $@
 	
 $(BUILD)%.o: $(SOURCE)/%.S
-	$(ARMGNU)-gcc $(COPS) -D__ASSEMBLY__ $< -c -o $@		
+	$(AS) $(COPS) -D__ASSEMBLY__ $< -c -o $@		
 
 $(TARGET): Makefile $(OBJECTS)
-	$(ARMGNU)-ar -r $(TARGET) $(OBJECTS)
-	$(ARMGNU)-objdump -D $(TARGET) | $(ARMGNU)-c++filt > $(LIST)
+	$(AR) -r $(TARGET) $(OBJECTS)
+	$(PREFIX)objdump -D $(TARGET) | $(PREFIX)c++filt > $(LIST)
 	
 # ARM v7	
 	
 $(BUILD7)%.o: $(SOURCE)/%.c
-	$(ARMGNU)-gcc $(COPS7) $< -c -o $@
+	$(CC) $(COPS7) $< -c -o $@
 	
 $(BUILD7)%.o: $(SOURCE)/%.cpp
-	$(ARMGNU)-g++ $(COPS7) -fno-rtti -std=c++11 $< -c -o $@		
+	$(CPP) $(COPS7) -fno-rtti -std=c++11 $< -c -o $@		
 	
 $(BUILD7)%.o: $(SOURCE)/%.S
-	$(ARMGNU)-gcc $(COPS7) -D__ASSEMBLY__ $< -c -o $@		
-
+	$(AS) $(COPS7) -D__ASSEMBLY__ $< -c -o $@	
+	
 $(TARGET7): Makefile $(OBJECTS7)
-	$(ARMGNU)-ar -r $(TARGET7) $(OBJECTS7)
-	$(ARMGNU)-objdump -D $(TARGET7) | $(ARMGNU)-c++filt > $(LIST7)
-	
-# ARM v8	
-
-$(BUILD8)%.o: $(SOURCE)/%.c
-	$(ARMGNU)-gcc $(COPS8) $< -c -o $@
-	
-$(BUILD8)%.o: $(SOURCE)/%.cpp
-	$(ARMGNU)-g++ $(COPS8) -fno-rtti -std=c++11 $< -c -o $@	
-	
-$(BUILD8)%.o: $(SOURCE)/%.S
-	$(ARMGNU)-gcc $(COPS) -D__ASSEMBLY__ $< -c -o $@		
-
-$(TARGET8): Makefile $(OBJECTS8)
-	$(ARMGNU)-ar -r $(TARGET8) $(OBJECTS8)
-	$(ARMGNU)-objdump -D $(TARGET8) | $(ARMGNU)-c++filt > $(LIST8)
-
-	
+	$(AR) -r $(TARGET7) $(OBJECTS7)
+	$(PREFIX)objdump -D $(TARGET7) | $(PREFIX)c++filt > $(LIST7)
