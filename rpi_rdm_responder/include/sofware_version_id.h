e24131aa0f824b61ce3b794e5e43f48185f73d79
Arjan van Vught <Arjan.van.Vught@gmail.com>
2017-08-08 09:54:33 +0200

    V1.9
    Now using lib-properties
    Using function sys instead of sys_time

--- rpi_rdm_responder/include/sofware_version_id.h
+++ rpi_rdm_responder/include/sofware_version_id.h
@@ -1,2 +1,2 @@
-// Generated za 8 jul 2017 15:55:25 CEST
-static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1499522125;
+// Generated di 8 aug 2017 9:34:26 CEST
+static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1502177666;
