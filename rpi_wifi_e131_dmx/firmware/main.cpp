f5fc66d3740624f0ac678491e3ab6a17cdf5bf90
Arjan van Vught <Arjan.van.Vught@gmail.com>
2017-12-31 15:49:31 +0100

    Revert "V2.0"
    This reverts commit 7e28a7bfab45e83a5de63f1fb12326138982eef3.

--- rpi_wifi_e131_dmx/firmware/main.cpp
+++ rpi_wifi_e131_dmx/firmware/main.cpp
@@ -29,24 +29,21 @@
 #include <uuid/uuid.h>
 
 #include "hardware.h"
-
 #include "console.h"
 #include "display.h"
 
-#include "wifi.h"
-#include "network.h"
+#include "ledblinktask.h"
 
 #include "e131bridge.h"
 #include "e131uuid.h"
 #include "e131params.h"
 
-#include "ledblinktask.h"
-
-// DMX output
+#include "dmxsender.h"
 #include "dmxparams.h"
-#include "dmxsend.h"
-// Monitor output
 #include "dmxmonitor.h"
+
+#include "wifi.h"
+#include "network.h"
 
 #include "util.h"
 
@@ -61,7 +58,7 @@ void notmain(void) {
 	E131Params e131params;
 	E131Uuid e131uuid;
 	DMXParams dmxparams;
-	DMXSend dmx;
+	DMXSender dmx;
 	DMXMonitor monitor;
 	Display display(0,8);
 	LedBlinkTask ledblinktask;
@@ -154,9 +151,9 @@ void notmain(void) {
 
 	if (tOutputType == OUTPUT_TYPE_DMX) {
 		printf("DMX Send parameters\n");
-		printf(" Break time   : %d\n", (int) dmx.GetDmxBreakTime());
-		printf(" MAB time     : %d\n", (int) dmx.GetDmxMabTime());
-		printf(" Refresh rate : %d\n", (int) (1E6 / dmx.GetDmxPeriodTime()));
+		printf(" Break time   : %d\n", (int) dmx.GetBreakTime());
+		printf(" MAB time     : %d\n", (int) dmx.GetMabTime());
+		printf(" Refresh rate : %d\n", (int) (1E6 / dmx.GetPeriodTime()));
 	}
 
 	if (IsOledConnected) {
