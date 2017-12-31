c83dee51db367f74146c5db0c7498ac7289211fc
Arjan van Vught <Arjan.van.Vught@gmail.com>
2017-12-31 15:47:04 +0100

    V2.3
    Built with the latest libraries

--- rpi_circle_artnet_dmx/kernel.cpp
+++ rpi_circle_artnet_dmx/kernel.cpp
@@ -40,11 +40,13 @@ extern void network_init(CNetSubSystem *);
 #include "network.h"
 #include "networkparams.h"
 
+// DMX output
 #include "dmxparams.h"
-#include "circle/dmxsender.h"
+#include "circle/dmxsend.h"
 
+// SPI WS28xx output
 #include "deviceparams.h"
-#include "circle/spisend.h"
+#include "spisend.h"
 
 #include "artnetnode.h"
 #include "artnetparams.h"
@@ -270,9 +272,9 @@ TShutdownMode CKernel::Run(void)
 
 	if (output_type == OUTPUT_TYPE_DMX) {
 		m_Logger.Write(FromKernel, LogNotice, "DMX Send parameters :");
-		m_Logger.Write(FromKernel, LogNotice, " Break time   : %u", m_DMX.GetBreakTime());
-		m_Logger.Write(FromKernel, LogNotice, " MAB time     : %u", m_DMX.GetMabTime());
-		m_Logger.Write(FromKernel, LogNotice, " Refresh rate : %u", (unsigned) (1000000 / m_DMX.GetPeriodTime()));
+		m_Logger.Write(FromKernel, LogNotice, " Break time   : %u", m_DMX.GetDmxBreakTime());
+		m_Logger.Write(FromKernel, LogNotice, " MAB time     : %u", m_DMX.GetDmxMabTime());
+		m_Logger.Write(FromKernel, LogNotice, " Refresh rate : %u", (unsigned) (1000000 / m_DMX.GetDmxPeriodTime()));
 	} else {
 		const TWS28XXType tType = m_SPI.GetLEDType();
 		m_Logger.Write(FromKernel, LogNotice, "Led stripe parameters :");
