f944c979d7193cd1c48e212aaba52135fd9c2410
Arjan van Vught <Arjan.van.Vught@gmail.com>
2018-10-13 09:54:59 +0200

    Minor improvements / cleanup

--- build_linux.sh
+++ build_linux.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-if which /opt/vc/bin/vcgencmd ; then
-	./makeall_linux-lib.sh 'DEF=-DRASPPI' clean 
-	./makeall_linux-lib.sh 'DEF=-DRASPPI'
-else
-	./makeall_linux-lib.sh clean 
-	./makeall_linux-lib.sh
-fi
+./makeall_linux-lib.sh clean 
+./makeall_linux-lib.sh
 
-if which /opt/vc/bin/vcgencmd ; then
-	./makeall_linux.sh 'DEF=-DRASPPI' clean 
-	./makeall_linux.sh 'DEF=-DRASPPI'
-else
-	./makeall_linux.sh clean 
-	./makeall_linux.sh 
-fi
+./makeall_linux.sh clean 
+./makeall_linux.sh 
--- makeall_linux-lib.sh
+++ makeall_linux-lib.sh
@@ -4,14 +4,14 @@ DIR=lib-*
 
 for f in $DIR
 do
-	echo -e "\e[32m[$f]\e[0m"
+	echo -e "\033[32m[$f]\033[0m"
 	cd "$f"
 	if [ -f Makefile.Linux ]; then
 		if [ $(grep -c bcm Makefile.Linux) -ne 0 ] || [ $(grep -c i2c  Makefile.Linux) -ne 0 ] ; then
 			if which /opt/vc/bin/vcgencmd ; then
 				make -f Makefile.Linux $1 $2 || exit
 			else
-				echo -e "\e[33mSkipping...\e[0m"
+				echo -e "\033[33mSkipping...\033[0m"
 				cd ..
 				continue
 			fi
@@ -21,7 +21,7 @@ do
 			if which /opt/vc/bin/vcgencmd ; then
 				make -f Makefile.Linux $1 $2 || exit
 			else
-				echo -e "\e[33mSkipping...\e[0m"
+				echo -e "\033[33mSkipping...\033[0m"
 				cd ..
 				continue
 			fi
