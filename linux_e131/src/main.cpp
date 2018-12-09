c24956419e4e3eb1a94a2bb66c5b364477d892b2
Arjan van Vught <Arjan.van.Vught@gmail.com>
2018-12-09 11:28:25 +0100

    V1.4
    Built with the latest libraries.
    The binary is built on Raspberry Pi Model 3

--- linux_e131/src/main.cpp
+++ linux_e131/src/main.cpp
@@ -27,13 +27,10 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
 #include "hardwarelinux.h"
 #include "networklinux.h"
+#include "ledblinklinux.h"
 
 #include "e131bridge.h"
 #include "e131uuid.h"
@@ -41,22 +38,32 @@
 
 #include "dmxmonitor.h"
 
+#if defined (RASPPI)
+ #include "spiflashstore.h"
+#endif
+
 #include "software_version.h"
 
 int main(int argc, char **argv) {
 	HardwareLinux hw;
 	NetworkLinux nw;
-	uint8_t nTextLength;
-	E131Params e131params;
-	DMXMonitor monitor;
-	E131Uuid e131uuid;
-	uuid_t uuid;
-	char uuid_str[UUID_STRING_LENGTH + 1];
+	LedBlinkLinux lb;
 
 	if (argc < 2) {
 		printf("Usage: %s ip_address|interface_name [max_dmx_channels]\n", argv[0]);
 		return -1;
 	}
+
+	uint8_t nTextLength;
+	printf("[V%s] %s %s Compiled on %s at %s\n", SOFTWARE_VERSION, hw.GetSysName(nTextLength), hw.GetVersion(nTextLength), __DATE__, __TIME__);
+	puts("sACN E1.31 Real-time DMX Monitor");
+
+	if (nw.Init(argv[1]) < 0) {
+		fprintf(stderr, "Not able to start the network\n");
+		return -1;
+	}
+
+	DMXMonitor monitor;
 
 	if (argc == 3) {
 		uint16_t max_channels = atoi(argv[2]);
@@ -66,48 +73,48 @@ int main(int argc, char **argv) {
 		monitor.SetMaxDmxChannels(max_channels);
 	}
 
+#if defined (RASPPI)
+	SpiFlashStore spiFlashStore;
+	E131Params e131params((E131ParamsStore *)spiFlashStore.GetStoreE131());
+	spiFlashStore.Dump();
+#else
+	E131Params e131params;
+#endif
+
 	if (e131params.Load()) {
 		e131params.Dump();
 	}
 
-	printf("[V%s] %s %s Compiled on %s at %s\n", SOFTWARE_VERSION, hw.GetSysName(nTextLength), hw.GetVersion(nTextLength), __DATE__, __TIME__);
-	puts("sACN E1.31 Real-time DMX Monitor");
-
-	if (nw.Init(argv[1]) < 0) {
-		fprintf(stderr, "Not able to start the network\n");
-		return -1;
-	}
+	uuid_t uuid;
 
 	if (e131params.isHaveCustomCid()) {
+		char uuid_str[UUID_STRING_LENGTH + 1];
 		memcpy(uuid_str, e131params.GetCidString(), UUID_STRING_LENGTH);
 		uuid_str[UUID_STRING_LENGTH] = '\0';
 		uuid_parse((const char *)uuid_str, uuid);
 	} else {
+		E131Uuid e131uuid;
 		e131uuid.GetHardwareUuid(uuid);
 	}
 
-	nw.Begin(E131_DEFAULT_PORT);
-
-	struct in_addr group_ip;
-	(void)inet_aton("239.255.0.0", &group_ip);
-	const uint16_t universe = e131params.GetUniverse();
-	group_ip.s_addr = group_ip.s_addr | ((uint32_t)(((uint32_t)universe & (uint32_t)0xFF) << 24)) | ((uint32_t)(((uint32_t)universe & (uint32_t)0xFF00) << 8));
-	nw.JoinGroup(group_ip.s_addr);
-
 	E131Bridge bridge;
 
-	bridge.setCid(uuid);
-	bridge.setUniverse(e131params.GetUniverse());
-	bridge.setMergeMode(e131params.GetMergeMode());
+	bridge.SetCid(uuid);
+	bridge.SetUniverse(e131params.GetUniverse());
+	bridge.SetMergeMode(e131params.GetMergeMode());
 	bridge.SetOutput(&monitor);
 
 	nw.Print();
-	puts("-------------------------------------------------------------------------------------------");
-	bridge.Print(group_ip.s_addr); //FIXME bridge.Print(group_ip.s_addr)
-	puts("-------------------------------------------------------------------------------------------");
+	bridge.Print();
+
+	bridge.Start();
+	lb.SetMode(LEDBLINK_MODE_NORMAL);
 
 	for (;;) {
 		(void) bridge.Run();
+#if defined (RASPPI)
+		spiFlashStore.Flash();
+#endif
 	}
 
 	return 0;
