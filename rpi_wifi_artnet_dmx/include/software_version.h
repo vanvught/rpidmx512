9c3bfdea7ea16183ccac8565c912e5122c8b9ba4
Arjan van Vught <Arjan.van.Vught@gmail.com>
2018-12-16 10:27:20 +0100

    V1.2 (Opi) / V3.0 (RPi)

--- rpi_wifi_artnet_dmx/include/software_version.h
+++ rpi_wifi_artnet_dmx/include/software_version.h
@@ -27,9 +27,9 @@
 #define SOFTWARE_VERSION_H_
 
 #if defined(H3)
- static const char SOFTWARE_VERSION[] = "1.1";
+ static const char SOFTWARE_VERSION[] = "1.2";
 #else
- static const char SOFTWARE_VERSION[] = "2.9";
+ static const char SOFTWARE_VERSION[] = "3.0";
 #endif
 
  #endif /* SOFTWARE_VERSION_H_ */
