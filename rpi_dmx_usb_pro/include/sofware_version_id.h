11665d159cfd33365dfb4160679b5143e027e15e
Arjan van Vught <Arjan.van.Vught@gmail.com>
2019-03-22 17:43:48 +0100

    V1.2 (OPi Zero) / V2.2 (RPi)

--- rpi_dmx_usb_pro/include/sofware_version_id.h
+++ rpi_dmx_usb_pro/include/sofware_version_id.h
@@ -1,2 +1,2 @@
-// Generated zo 3 mrt 2019 11:54:58 CET
-static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1551610498;
+// Generated vr 22 mrt 2019 17:40:47 CET
+static const uint32_t DEVICE_SOFTWARE_VERSION_ID=1553272847;
