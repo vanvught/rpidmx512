6b008cff7b274e12167ffa03c9896f03a8a99c58
Arjan <Arjan.van.Vught@gmail.com>
2019-12-13 14:40:07 +0100

    Revert "V1.2"
    This reverts commit 89b66ae7942e667eb8c11544d5d0a31497857d84.

--- opi_midi_monitor/firmware/main.cpp
+++ opi_midi_monitor/firmware/main.cpp
@@ -2,7 +2,7 @@
  * @file main.cpp
  *
  */
-/* Copyright (C) 2016-2019 by Arjan van Vught mailto:info@raspberrypi-dmx.nl
+/* Copyright (C) 2016-2018 by Arjan van Vught mailto:info@raspberrypi-dmx.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -46,6 +46,7 @@ void notmain(void) {
 	MidiMonitor monitor;
 
 	midi_params_init();
+	midi_set_interface(midi_params_get_interface());
 	midi_set_baudrate(midi_params_get_baudrate());
 	midi_active_set_sense(true);
 	midi_init(MIDI_DIRECTION_INPUT);
