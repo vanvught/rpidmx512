6e1043de39684b925ee692e61a1369463e0de5c8
Arjan <Arjan.van.Vught@gmail.com>
2019-12-13 14:41:15 +0100

    V1.2

--- opi_midi_monitor/include/software_version.h
+++ opi_midi_monitor/include/software_version.h
@@ -26,6 +26,6 @@
 #ifndef SOFTWARE_VERSION_H_
 #define SOFTWARE_VERSION_H_
 
-static const char SOFTWARE_VERSION[] = "1.1";
+static const char SOFTWARE_VERSION[] = "1.2";
 
 #endif /* SOFTWARE_VERSION_H_ */
