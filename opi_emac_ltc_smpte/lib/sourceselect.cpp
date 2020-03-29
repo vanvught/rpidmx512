5a14090f670aa8e6d14d5e59e216922e7b3bb4d3
Arjan <Arjan.van.Vught@gmail.com>
2020-03-29 12:23:10 +0200

    V2.5

--- opi_emac_ltc_smpte/lib/sourceselect.cpp
+++ opi_emac_ltc_smpte/lib/sourceselect.cpp
@@ -2,7 +2,7 @@
  * @file sourceselect.cpp
  *
  */
-/* Copyright (C) 2019-2020 by Arjan van Vught mailto:info@raspberrypi-dmx.nl
+/* Copyright (C) 2019-2020 by Arjan van Vught mailto:info@orangepi-dmx.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
