35a79de51d03f66d0646b1d07c9bb324e3bfc13b
Arjan <Arjan.van.Vught@gmail.com>
2020-05-01 22:10:40 +0200

    V1.8

--- linux_osc/src/main.cpp
+++ linux_osc/src/main.cpp
@@ -2,7 +2,7 @@
  * @file main.cpp
  *
  */
-/* Copyright (C) 2017-2019 by Arjan van Vught mailto:info@raspberrypi-dmx.nl
+/* Copyright (C) 2017-2020 by Arjan van Vught mailto:info@raspberrypi-dmx.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -35,17 +35,19 @@
 
 #include "oscserver.h"
 #include "oscserverparms.h"
+#include "storeoscserver.h"
 
 #include "dmxmonitor.h"
 #include "dmxmonitorparams.h"
+#include "storemonitor.h"
 
-#if defined (RASPPI)
- #include "spiflashstore.h"
- #include "storeoscserver.h"
-#endif
+#include "spiflashstore.h"
+
+#include "remoteconfig.h"
+#include "remoteconfigparams.h"
+#include "storeremoteconfig.h"
 
 #include "firmwareversion.h"
-
 #include "software_version.h"
 
 int main(int argc, char **argv) {
@@ -58,24 +60,6 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-#if defined (RASPPI)
-	SpiFlashStore spiFlashStore;
-	StoreOscServer storeOscServer;
-
-	OSCServerParams oscparms((OSCServerParamsStore *)&storeOscServer);
-
-	spiFlashStore.Dump();
-#else
-	OSCServerParams oscparms;
-#endif
-
-	OscServer server;
-
-	if (oscparms.Load()) {
-		oscparms.Dump();
-		oscparms.Set(&server);
-	}
-
 	fw.Print();
 
 	puts("OSC Real-time DMX Monitor");
@@ -85,33 +69,50 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	Handler handler;
-	server.SetOscServerHandler(&handler);
+	SpiFlashStore spiFlashStore;
+	StoreOscServer storeOscServer;
 
-	DMXMonitor monitor;
-	DMXMonitorParams params;
+	OSCServerParams oscparms(&storeOscServer);
+	OscServer server;
 
-	if (params.Load()) {
-		params.Dump();
-		params.Set(&monitor);
+	if (oscparms.Load()) {
+		oscparms.Dump();
+		oscparms.Set(&server);
 	}
 
+	DMXMonitor monitor;
+	DMXMonitorParams monitorParams(new StoreMonitor);
+
+	if (monitorParams.Load()) {
+		monitorParams.Dump();
+		monitorParams.Set(&monitor);
+	}
+
+	server.SetOscServerHandler(new Handler);
 	server.SetOutput(&monitor);
 
 	nw.Print();
 	server.Print();
 
-	server.Start();
+	RemoteConfig remoteConfig(REMOTE_CONFIG_OSC, REMOTE_CONFIG_MODE_MONITOR, 1);
 
-#if defined (RASPPI)
+	StoreRemoteConfig storeRemoteConfig;
+	RemoteConfigParams remoteConfigParams(&storeRemoteConfig);
+
+	if(remoteConfigParams.Load()) {
+		remoteConfigParams.Set(&remoteConfig);
+		remoteConfigParams.Dump();
+	}
+
 	while (spiFlashStore.Flash())
 		;
 
-	spiFlashStore.Dump();
-#endif
+	server.Start();
 
 	for (;;) {
 		server.Run();
+		remoteConfig.Run();
+		spiFlashStore.Flash();
 	}
 
 	return 0;
