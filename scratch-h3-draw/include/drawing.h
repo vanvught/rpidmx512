0db757a6236ca4263ba3c334032d22c092b6b068
hippyau <dmxout@gmail.com>
2020-08-13 11:47:09 +1000

    Update

--- scratch-h3-draw/include/drawing.h
+++ scratch-h3-draw/include/drawing.h
@@ -1,3 +1,35 @@
+/**
+ * @file drawing.h
+ *
+ */
+
+/*
+ * Drawing functions for the FB console.
+ *
+*/
+
+/*  Copyright (C) 2019-2020 by hippy mailto:dmxout@gmail.com
+ *  Copyright (C) 2019-2020 by Arjan van Vught mailto:info@orangepi-dmx.nl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ */
 #ifndef DRAWING_H_
 #define DRAWING_H_
 
@@ -6,33 +38,34 @@
 #endif
 
 
-#include "h3.h"
-#include "h3/console_fb.h"
 #include "device/fb.h"
 
 
 class Drawing {
+  
 public:
-	Drawing(void) {	s_pThis = this; }
-	~Drawing(void) { }
+    Drawing() {};
+    
+    void pixel(uint32_t x, uint32_t y, uint32_t c); 
+    void pixel(int32_t x, int32_t y, uint32_t c);   
+    void line(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, uint32_t p);
+    void rect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t p);
+    void fillRect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t p);    
+    void circle(uint32_t xc, uint32_t yc, uint32_t radius, uint32_t p);
+    void fillCircle(uint32_t x, uint32_t y, uint32_t radius, uint32_t p);
+    void triangle(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, uint32_t x3, uint32_t y3, uint32_t p);
+   // void fillTriangle(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, uint32_t x3, uint32_t y3, uint32_t p);
 
-    void pixel(int32_t x, int32_t y, uint32_t c); 
-    void line(int32_t x1, int32_t y1, int32_t x2, int32_t y2, uint32_t p, uint32_t pattern);
-    void rect(int32_t x, int32_t y, int32_t w, int32_t h, uint32_t p);
-    void fillRect(int32_t x, int32_t y, int32_t w, int32_t h, uint32_t p);
-    void circle(int32_t x, int32_t y, int32_t radius, uint32_t p, uint8_t mask);
-    void fillCircle(int32_t x, int32_t y, int32_t radius, uint32_t p);
-    void triangle(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t x3, int32_t y3, uint32_t p);
-    void fillTriangle(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t x3, int32_t y3, uint32_t p);
-
-	static Drawing *Get(void) { return s_pThis;	}
+	static Drawing *Get() {
+		return s_pThis;
+	}
 
 private:
-	static Drawing *s_pThis;
+    uint32_t fbW(void) { return (fb_width); }
+    uint32_t fbH(void) { return (fb_height); }
 
-    static int32_t fbW(void) { return static_cast<int32_t>(fb_width); }
-    static int32_t fbH(void) { return static_cast<int32_t>(fb_height); }
-    
+    static Drawing *s_pThis;
 };
+
 
 #endif
--- scratch-h3-draw/lib/drawing.cpp
+++ scratch-h3-draw/lib/drawing.cpp
@@ -1,4 +1,4 @@
-#if defined ORANGE_PI_ONE
+#if defined (ORANGE_PI_ONE)
 /**
  * @file drawing.cpp
  *
@@ -6,13 +6,11 @@
 
 /*
  * Drawing functions for the FB console.
- * 
- * Primative shape drawing functions 
- * Heavily inspired by javidx9's olcPixelGameEngine (https://github.com/OneLoneCoder/olcPixelGameEngine)
  *
 */
 
-/* Copyright (C) 2019-2020 by Arjan van Vught mailto:info@orangepi-dmx.nl
+/*  Copyright (C) 2019-2020 by hippy mailto:dmxout@gmail.com
+ *  Copyright (C) 2019-2020 by Arjan van Vught mailto:info@orangepi-dmx.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -34,198 +32,126 @@
  * 
  */
 
-
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include "drawing.h"
 
+#include "h3/console_fb.h"  // console_putpixel()
 #include "device/fb.h"
 
 
-#define SWAP(x,y) do \
-    { unsigned char swap_temp[sizeof(x) == sizeof(y) ? static_cast<signed>(sizeof(x)) : -1]; \
-     memcpy(swap_temp,&y,sizeof(x)); \
-     memcpy(&y,&x,       sizeof(x)); \
-     memcpy(&x,swap_temp,sizeof(x)); \
-    } while(0)
+Drawing *Drawing::s_pThis = 0;
 
+void Drawing::pixel(uint32_t x, uint32_t y, uint32_t c) {
+        console_putpixel(x, y, c);
+}
 
 void Drawing::pixel(int32_t x, int32_t y, uint32_t c) {
         console_putpixel(static_cast<uint32_t>(x), static_cast<uint32_t>(y), c);
 }
 
-void Drawing::line(int32_t x1, int32_t y1, int32_t x2, int32_t y2, uint32_t p, uint32_t pattern) {
-    int32_t x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
-    dx = x2 - x1; dy = y2 - y1;
+void Drawing::line(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, uint32_t p) { 
 
-    auto rol = [&](void) { 
-        pattern = (pattern << 1) | (pattern >> 31); 
-        return pattern & 1; 
+    int32_t dx = abs(static_cast<int32_t>(x2 - x1));
+    uint32_t sx = x1 < x2 ? 1U : -1U;
+    int32_t dy = abs(static_cast<int32_t>(y2 - y1));
+    uint32_t sy = y1 < y2 ? 1U : -1U;
+    int32_t err = (dx > dy ? dx : -dy) / 2;
+
+    while (pixel(x1, y1, p), x1 != x2 || y1 != y2) {
+        int32_t e2 = err;
+        if (e2 > -dx) { err -= dy; x1 += sx; }
+        if (e2 <  dy) { err += dx; y1 += sy; }
+    }
+}
+
+void Drawing::rect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t p) {
+    line(x, y, x + w, y, p);
+    line(x + w, y, x + w, y + h, p);
+    line(x + w, y + h, x, y + h, p);
+    line(x, y + h, x, y, p);
+}
+
+void Drawing::fillRect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t p) {
+
+    uint32_t x2 = x + w;
+    uint32_t y2 = y + h;
+
+    if (x >= fbW()) { x = fbW(); }
+    if (y >= fbH()) { y = fbH(); }
+    if (x2 >= fbW()) { x2 = fbW(); }
+    if (y2 >= fbH()) { y2 = fbH(); }
+    for (unsigned int i = x; i < x2; i++) {
+        for (unsigned int j = y; j < y2; j++) {
+            pixel(i, j, p);
+        }
+    }     
+}
+
+// Function for circle-generation using Bresenham's algorithm 
+void Drawing::circle(uint32_t xc, uint32_t yc, uint32_t radius, uint32_t p) { 
+    
+    uint32_t x = 0, y = radius; 
+    int32_t d = 3 - 2 * static_cast<int>(radius); 
+   
+    auto drawcirc = [&](uint32_t xc, uint32_t yc, uint32_t p) {
+        pixel(xc+x, yc+y, p); 
+        pixel(xc-x, yc+y, p); 
+        pixel(xc+x, yc-y, p); 
+        pixel(xc-x, yc-y, p); 
+        pixel(xc+y, yc+x, p); 
+        pixel(xc-y, yc+x, p); 
+        pixel(xc+y, yc-x, p); 
+        pixel(xc-y, yc-x, p);   
     };
 
-    // Line is vertical
-    if (dx == 0) {
-        if (y2 < y1) SWAP(y1, y2);
-        for (y = y1; y <= y2; y++) if (rol()) pixel(x1, y, p);
-        return;
-    }
-    // Line is horizontal
-    if (dy == 0) {
-        if (x2 < x1) SWAP(x1, x2);
-        for (x = x1; x <= x2; x++) if (rol()) pixel(x, y1, p);
-        return;
-    }
-
-    dx1 = abs(dx); 
-    dy1 = abs(dy);
-    px = 2 * dy1 - dx1;
-    py = 2 * dx1 - dy1;
-    if (dy1 <= dx1) {
-        if (dx >= 0) {
-            x = x1; y = y1; xe = x2;
+    drawcirc(xc, yc, p);
+    while (y >= x) { 
+        x++; 
+        if (d > 0) { 
+            y--;  
+            d = d + 4 * static_cast<int32_t>(x - y) + 10; 
         } else {
-            x = x2; y = y2; xe = x1;
-        }			
-        if (rol()) pixel(x, y, p);
-        for (i = 0; x < xe; i++) {
-            x = x + 1;
-            if (px < 0)
-                px = px + 2 * dy1;
-            else {
-                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) y = y + 1; else y = y - 1;
-                px = px + 2 * (dy1 - dx1);
-            }
-            if (rol()) pixel(x, y, p);
+            d = d + 4 * static_cast<int32_t>(x) + 6; 
         }
-    }
-    else {
-        if (dy >= 0) {
-            x = x1; 
-            y = y1; 
-            ye = y2;
-        }
-        else {
-            x = x2; 
-            y = y2; 
-            ye = y1;
-        }
-        if (rol()) pixel(x, y, p);
-        for (i = 0; y < ye; i++) {
-            y = y + 1;
-            if (py <= 0)
-                py = py + 2 * dx1;
-            else {
-                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) x = x + 1; else x = x - 1;
-                py = py + 2 * (dx1 - dy1);
-            }
-            if (rol()) pixel(x, y, p);
-        }
-    }
-}
+        drawcirc(xc, yc, p);
+    } 
+} 
 
-void Drawing::rect(int32_t x, int32_t y, int32_t w, int32_t h, uint32_t p) {
-    line(x, y, x + w, y, p, 0);
-    line(x + w, y, x + w, y + h, p, 0);
-    line(x + w, y + h, x, y + h, p, 0);
-    line(x, y + h, x, y, p, 0);
-}
+void Drawing::fillCircle(uint32_t x, uint32_t y, uint32_t radius, uint32_t p) {
 
-void Drawing::circle(int32_t x, int32_t y, int32_t radius, uint32_t p, uint8_t mask) { 
+    int ox = static_cast<int>(x);
+    int oy = static_cast<int>(y);
+    int rad = static_cast<int>(radius);
 
-    if (radius < 0 || x < -radius || y < -radius || x - fbW() > radius || y - fbH() > radius)
-        return;
-
-    if (radius > 0) {
-        int x0 = 0;
-        int y0 = radius;
-        int d = 3 - 2 * radius;
-
-        // only formulate 1/8 of circle
-        while (y0 >= x0) { 
-            // Draw even octants
-            if (mask & 0x01) pixel(x + x0, y - y0, p);// Q6 - upper right right
-            if (mask & 0x04) pixel(x + y0, y + x0, p);// Q4 - lower lower right
-            if (mask & 0x10) pixel(x - x0, y + y0, p);// Q2 - lower left left
-            if (mask & 0x40) pixel(x - y0, y - x0, p);// Q0 - upper upper left
-            if (x0 != 0 && x0 != y0) {
-                if (mask & 0x02) pixel(x + y0, y - x0, p);// Q7 - upper upper right
-                if (mask & 0x08) pixel(x + x0, y + y0, p);// Q5 - lower right right
-                if (mask & 0x20) pixel(x - y0, y + x0, p);// Q3 - lower lower left
-                if (mask & 0x80) pixel(x - x0, y - y0, p);// Q1 - upper left left
-            }
-            if (d < 0)
-                d += 4 * x0++ + 6;
-            else
-                d += 4 * (x0++ - y0--) + 10;
-        }
-    }
-    else
-        pixel(x, y, p);
-}
-
-void Drawing::fillRect(int32_t x, int32_t y, int32_t w, int32_t h, uint32_t p) {
-
-    int32_t x2 = x + w;
-    int32_t y2 = y + h;
-
-    if (x < 0) x = 0;
-    if (x >= fbW()) x = fbW();
-    if (y < 0) y = 0;
-    if (y >= fbH()) y = fbH();
-    if (x2 < 0) x2 = 0;
-    if (x2 >= fbW()) x2 = fbW();
-    if (y2 < 0) y2 = 0;
-    if (y2 >= fbH()) y2 = fbH();
-
-    for (int i = x; i < x2; i++)
-        for (int j = y; j < y2; j++)
-            pixel(i, j, p);
-}
-
-void Drawing::fillCircle(int32_t x, int32_t y, int32_t radius, uint32_t p) { 
-
-    if (radius < 0 || x < -radius || y < -radius || x - fbW() > radius || y - fbH() > radius)
-        return;
-
-    if (radius > 0) {
-        int x0 = 0;
-        int y0 = radius;
-        int d = 3 - 2 * radius;
-
-        auto drawline = [&](int sx, int ex, int y) {
-            for (int x = sx; x <= ex; x++)
-                pixel(x, y, p);
-        };
-
-        while (y0 >= x0) {
-            drawline(x - y0, x + y0, y - x0);
-            if (x0 > 0)	drawline(x - y0, x + y0, y + x0);
-            if (d < 0)
-                d += 4 * x0++ + 6;
-            else {
-                if (x0 != y0) {
-                    drawline(x - x0, x + x0, y - y0);
-                    drawline(x - x0, x + x0, y + y0);
-                }
-                d += 4 * (x0++ - y0--) + 10;
+    for(int y=-rad; y<=rad; y++){ 
+        for(int x=-rad; x<=rad; x++) {
+            if(x*x+y*y <= rad*rad) {
+                pixel(ox+x, oy+y, p);
             }
         }
     }
-    else
-        pixel(x, y, p);
+}
+
+void Drawing::triangle(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, uint32_t x3, uint32_t y3, uint32_t p) {
+
+    line(x1, y1, x2, y2, p);
+    line(x2, y2, x3, y3, p);
+    line(x3, y3, x1, y1, p);
 }
 
 
-void Drawing::triangle(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t x3, int32_t y3, uint32_t p) {
-    line(x1, y1, x2, y2, p, 0);
-    line(x2, y2, x3, y3, p, 0);
-    line(x3, y3, x1, y1, p, 0);
-}
+/* 
 
+template<typename T> void swap(T& t1, T& t2) {
+    T tmp(t1);
+    t1=t2;
+    t2=tmp;
+}
 
 // https://www.avrfreaks.net/sites/default/files/triangles.c
-void Drawing::fillTriangle(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t x3, int32_t y3, uint32_t p) {
+void Drawing::fillTriangle(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, uint32_t x3, uint32_t y3, uint32_t p) {
     
     auto drawline = [&](int sx, int ex, int ny) { for (int i = sx; i <= ex; i++) pixel(i, ny, p); };
     
@@ -235,9 +161,9 @@ void Drawing::fillTriangle(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32
     int signx1, signx2, dx1, dy1, dx2, dy2;
     int e1, e2;
     // Sort vertices
-    if (y1 > y2) { SWAP(y1, y2); SWAP(x1, x2); }
-    if (y1 > y3) { SWAP(y1, y3); SWAP(x1, x3); }
-    if (y2 > y3) { SWAP(y2, y3); SWAP(x2, x3); }
+    if (y1 > y2) { swap(y1, y2); swap(x1, x2); }
+    if (y1 > y3) { swap(y1, y3); swap(x1, x3); }
+    if (y2 > y3) { swap(y2, y3); swap(x2, x3); }
 
     t1x = t2x = x1; y = y1;   // Starting points
     dx1 = (x2 - x1);
@@ -250,8 +176,8 @@ void Drawing::fillTriangle(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32
     else signx2 = 1;
     dy2 = (y3 - y1);
 
-    if (dy1 > dx1) { SWAP(dx1, dy1); changed1 = true; }
-    if (dy2 > dx2) { SWAP(dy2, dx2); changed2 = true; }
+    if (dy1 > dx1) { swap(dx1, dy1); changed1 = true; }
+    if (dy2 > dx2) { swap(dy2, dx2); changed2 = true; }
 
     e2 = (dx2 >> 1);
     // Flat top, just process the second half
@@ -310,7 +236,7 @@ next:
     t1x = x2;
 
     if (dy1 > dx1) {   // swap values
-        SWAP(dy1, dx1);
+        swap(dy1, dx1);
         changed1 = true;
     }
     else changed1 = false;
@@ -360,5 +286,5 @@ next:
         if (y > y3) return;
     }
 }
-
+ */
 #endif
