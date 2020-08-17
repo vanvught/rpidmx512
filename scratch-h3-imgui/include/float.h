9dacc57e6fbc832c5972b5d8825ae7483d774e15
hippyau <dmxout@gmail.com>
2020-08-17 21:19:12 +1000

    experimental port imgui attempt

---
+++ scratch-h3-imgui/include/float.h
@@ -0,0 +1,14 @@
+
+
+#ifndef _FLOAT_H___
+#define _FLOAT_H___
+
+
+
+#define FLT_MAX		__FLT_MAX__
+#define DBL_MAX		__DBL_MAX__
+#define LDBL_MAX	__LDBL_MAX__
+
+
+
+#endif
---
+++ scratch-h3-imgui/include/strtod.h
@@ -0,0 +1,15 @@
+
+#ifndef _STRTOD_H
+#define _STRTOD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern double strtod(const char *str, char **endptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
