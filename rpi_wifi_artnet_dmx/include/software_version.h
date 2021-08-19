efa47d50dfcb833485bb02ba7fd8dde2e17e4b3b
Arjan van Vught <arjan.van.vught@gmail.com>
2021-08-19 17:43:19 +0200

    Updated SOFTWARE_VERSION

--- rpi_wifi_artnet_dmx/include/software_version.h
+++ rpi_wifi_artnet_dmx/include/software_version.h
@@ -27,7 +27,7 @@
 #define SOFTWARE_VERSION_H_
 
 #if defined(H3)
- constexpr char SOFTWARE_VERSION[] = "1.8";
+ constexpr char SOFTWARE_VERSION[] = "1.9";
 #else
  constexpr char SOFTWARE_VERSION[] = "3.4";
 #endif
