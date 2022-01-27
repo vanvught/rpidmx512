d5d020f575288dcad82de413d0e4655b7fe1ab2b
Arjan van Vught <arjan.van.vught@gmail.com>
2022-01-27 16:31:37 +0100

    Added define CONSOLE_NULL

--- lib-hal/debug/i2cdetect.cpp
+++ lib-hal/debug/i2cdetect.cpp
@@ -2,7 +2,7 @@
  * @file i2cdetect.cpp
  *
  */
-/* Copyright (C) 2020-2021 by Arjan van Vught mailto:info@orangepi-dmx.nl
+/* Copyright (C) 2020-2022 by Arjan van Vught mailto:info@orangepi-dmx.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,47 +29,41 @@
 
 #include "hal_i2c.h"
 
-#if defined(BARE_METAL)
-# include "uart0_debug.h"
-# define printf uart0_printf
-# define puts(s) uart0_puts(s);uart0_putc('\n')
-#endif
+inline static bool i2c_is_connected(uint8_t nAddress) {
+	uint8_t nResult;
+	char buffer;
 
-inline static bool i2c_is_connected(uint8_t address) {
-	uint8_t ret;
-	char buf;
+	FUNC_PREFIX(i2c_set_address(nAddress));
 
-	FUNC_PREFIX(i2c_set_address(address));
-
-	if ((address >= 0x30 && address <= 0x37) || (address >= 0x50 && address <= 0x5F)) {
-		ret = FUNC_PREFIX(i2c_read(&buf, 1));
+	if ((nAddress >= 0x30 && nAddress <= 0x37) || (nAddress >= 0x50 && nAddress <= 0x5F)) {
+		nResult = FUNC_PREFIX(i2c_read(&buffer, 1));
 	} else {
 		/* This is known to corrupt the Atmel AT24RF08 EEPROM */
-		ret = FUNC_PREFIX(i2c_write(nullptr, 0));
+		nResult = FUNC_PREFIX(i2c_write(nullptr, 0));
 	}
 
-	return (ret == 0) ? true : false;
+	return (nResult == 0) ? true : false;
 }
 
-I2cDetect::I2cDetect() {
-	uint8_t first = 0x03, last = 0x77;
-	uint8_t i, j;
+static constexpr uint32_t FIRST = 0x03;
+static constexpr uint32_t LAST = 0x77;
 
+I2cDetect::I2cDetect() {
 	FUNC_PREFIX(i2c_set_baudrate(100000));
 
 	puts("\n     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f");
 
-	for (i = 0; i < 128; i = static_cast<uint8_t>(i + 16)) {
+	for (uint32_t i = 0; i < 128; i = (i + 16)) {
 		printf("%02x: ", i);
-		for (j = 0; j < 16; j++) {
+		for (uint32_t j = 0; j < 16; j++) {
 			/* Skip unwanted addresses */
-			if (i + j < first || i + j > last) {
+			if ((i + j < FIRST) || (i + j > LAST)) {
 				printf("   ");
 				continue;
 			}
 
 			if (i2c_is_connected(static_cast<uint8_t>(i + j))) {
-				printf("%02x ", (i + j));
+				printf("%02x ", i + j);
 			} else {
 				printf("-- ");
 			}
