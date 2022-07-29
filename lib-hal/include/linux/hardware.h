45be58dc1085f75bf9dd3ceffc5fa436f847c031
Arjan van Vught <arjan.van.vught@gmail.com>
2022-07-29 12:19:52 +0200

    Changed reboot handler implementation

--- lib-hal/include/linux/hardware.h
+++ lib-hal/include/linux/hardware.h
@@ -2,7 +2,7 @@
  * @file hardware.h
  *
  */
-/* Copyright (C) 2020-2021 by Arjan van Vught mailto:info@orangepi-dmx.nl
+/* Copyright (C) 2020-2022 by Arjan van Vught mailto:info@orangepi-dmx.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
