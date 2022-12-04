e9545fd3e17f88e3714e2c6f0d71649c2690276b
Arjan van Vught <arjan.van.vught@gmail.com>
2022-12-04 17:47:03 +0100

    Updated SOFTWARE_VERSION[]

--- linux_artnet/include/software_version.h
+++ linux_artnet/include/software_version.h
@@ -26,6 +26,6 @@
 #ifndef SOFTWARE_VERSION_H_
 #define SOFTWARE_VERSION_H_
 
-static const char SOFTWARE_VERSION[] = "2.2";
+static const char SOFTWARE_VERSION[] = "2.3";
 
 #endif /* SOFTWARE_VERSION_H_ */
