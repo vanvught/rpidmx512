d6dd1831be32d8589b084d6a20257e39bc6388e3
Arjan van Vught <arjan.van.vught@gmail.com>
2023-07-24 20:56:56 +0200

    Removed lib-dmxartnet, lib-dmxe131

--- lib-dmx/src/e131/dmxinput.cpp
+++ lib-dmx/src/e131/dmxinput.cpp
@@ -2,7 +2,7 @@
  * @file dmxinput.cpp
  *
  */
-/* Copyright (C) 2020-2021 by Arjan van Vught mailto:info@orangepi-dmx.nl
+/* Copyright (C) 2020-2023 by Arjan van Vught mailto:info@orangepi-dmx.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -89,4 +89,4 @@ const uint8_t *dmx_handler(const uint32_t nPortIndex, uint32_t& nLength, uint32_
 	nLength = 0;
 	return nullptr;
 }
-}  // namespace e131bridge
+}  // namespace e131
