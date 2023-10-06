c92deb17045b5e1036043706f56548f09b1fecc0
Arjan van Vught <arjan.van.vught@gmail.com>
2023-10-06 15:49:37 +0200

    phy_customized_status is now using ETHTOOL_GLINKSETTINGS
    *. Linux only. It is not working on macOS, yet.

--- lib-network/src/linux/net_phy.cpp
+++ lib-network/src/linux/net_phy.cpp
@@ -24,18 +24,114 @@
  */
 
 #include <cstdint>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <cstdint>
+#if !defined(__APPLE__)
+# include <errno.h>
+# include <unistd.h>
+# include <sys/socket.h>
+# include <sys/ioctl.h>
+# include <net/if.h>
+# include <linux/ethtool.h>
+# include <linux/sockios.h>
+#endif
+
+#include "network.h"
+#include "debug.h"
+
+#if !defined(__APPLE__)
+static struct ethtool_link_settings *ethtoolLinkSettings;
+
+static int _phy_customized_status() {
+	DEBUG_ENTRY
+
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if (fd < 0) {
+		perror("socket");
+		DEBUG_EXIT
+		return -1;
+	}
+
+	struct ifreq ifr;
+	memset(&ifr, 0, sizeof(ifr));
+	strcpy(ifr.ifr_name, Network::Get()->GetIfName());
+
+	char buffer[sizeof(struct ethtool_link_settings) + sizeof(uint32_t) * 3 * 128];
+	ethtoolLinkSettings = (struct ethtool_link_settings *)buffer;
+	memset(buffer, 0, sizeof(buffer));
+
+	ethtoolLinkSettings->cmd = ETHTOOL_GLINKSETTINGS;
+	ifr.ifr_data = (caddr_t)ethtoolLinkSettings;
+
+	if (ioctl(fd, SIOCETHTOOL, &ifr) == -1) {
+		perror("ioctl");
+		close(fd);
+		DEBUG_EXIT
+		return -1;
+	}
+
+	if (ethtoolLinkSettings->link_mode_masks_nwords >= 0) {
+		close(fd);
+		DEBUG_EXIT
+		return -1;
+	}
+
+	ethtoolLinkSettings->cmd = ETHTOOL_GLINKSETTINGS;
+	ethtoolLinkSettings->link_mode_masks_nwords = -ethtoolLinkSettings->link_mode_masks_nwords;
+
+	if (ioctl(fd, SIOCETHTOOL, &ifr) == -1)  {
+		perror("ioctl");
+		close(fd);
+		DEBUG_EXIT
+		return -1;
+	}
+
+	DEBUG_PRINTF("ethtool_link_settings.speed = %i", ethtoolLinkSettings->speed);
+	DEBUG_PRINTF("ethtool_link_settings.duplex = %i",  ethtoolLinkSettings->duplex);
+	DEBUG_PRINTF("ethtool_link_settings.autoneg = %i", ethtoolLinkSettings->autoneg);
+
+	close(fd);
+	DEBUG_EXIT
+	return 0;
+}
+
+#undef DUPLEX_HALF
+#undef DUPLEX_FULL
+#endif
 
 #include "emac/phy.h"
 
-#include "debug.h"
-
 namespace net {
-
 void phy_customized_status(PhyStatus& phyStatus) {
-	phyStatus.link  = Link::STATE_UP;
+	phyStatus.link = Link::STATE_UP;
 	phyStatus.duplex = Duplex::DUPLEX_FULL;
 	phyStatus.speed = Speed::SPEED1000;
 	phyStatus.bAutonegotiation = true;
 
+#if !defined(__APPLE__)
+	if (_phy_customized_status() < 0) {
+		return;
+	}
+
+	switch (ethtoolLinkSettings->speed) {
+	case 10:
+		phyStatus.speed = Speed::SPEED10;
+		break;
+	case 100:
+		phyStatus.speed = Speed::SPEED100;
+		break;
+	case 1000:
+		phyStatus.speed = Speed::SPEED1000;
+		break;
+	default:
+		break;
+	}
+
+	phyStatus.duplex = (ethtoolLinkSettings->duplex == 1) ? Duplex::DUPLEX_FULL : Duplex::DUPLEX_HALF;
+	phyStatus.bAutonegotiation = (ethtoolLinkSettings->autoneg == 1);
+#endif
 }
 }  // namespace net
