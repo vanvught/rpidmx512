2424ec553c50dcc215011d3d67a48f0e6f677986
Arjan van Vught <arjan.van.vught@gmail.com>
2024-03-20 20:14:34 +0100

    Updated build configuration

--- lib-tlc59711dmx/Rules.mk
+++ lib-tlc59711dmx/Rules.mk
@@ -1,13 +1,13 @@
 EXTRA_INCLUDES+=../lib-tlc59711/include ../lib-lightset/include ../lib-properties/include 
 
 ifneq ($(MAKE_FLAGS),)
-	ifneq (,$(findstring ENABLE_RDM_MANUFACTURER_PIDS,$(MAKE_FLAGS)))
+	ifneq (,$(findstring CONFIG_RDM_ENABLE_MANUFACTURER_PIDS,$(MAKE_FLAGS)))
 		EXTRA_INCLUDES+=../lib-rdm/include
 		EXTRA_SRCDIR+=src/rdm
 	endif
 else
 	DEFINES+=OUTPUT_DMX_TLC59711
-	DEFINES+=ENABLE_RDM_MANUFACTURER_PIDS
+	DEFINES+=CONFIG_RDM_ENABLE_MANUFACTURER_PIDS
 	EXTRA_INCLUDES+=../lib-rdm/include
 	EXTRA_SRCDIR+=src/rdm
 endif
