7387f6c62d932ce18b89f4d1614b130925092720
Arjan van Vught <arjan.van.vught@gmail.com>
2025-02-06 11:25:23 +0100

    Updated SOFTWARE_VERSION[]

--- opi_emac_osc_monitor/include/software_version.h
+++ opi_emac_osc_monitor/include/software_version.h
@@ -2,7 +2,7 @@
  * @file software_version.h
  *
  */
-/* Copyright (C) 2019-2024 by Arjan van Vught mailto:info@gd32-dmx.org
+/* Copyright (C) 2019-2025 by Arjan van Vught mailto:info@gd32-dmx.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,6 +26,6 @@
 #ifndef SOFTWARE_VERSION_H_
 #define SOFTWARE_VERSION_H_
 
-constexpr char SOFTWARE_VERSION[] = "3.5";
+constexpr char SOFTWARE_VERSION[] = "3.6";
 
 #endif /* SOFTWARE_VERSION_H_ */
