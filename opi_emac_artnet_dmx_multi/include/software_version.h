524f16c77631286e186c52933aed3d43c69a5d43
Arjan van Vught <arjan.van.vught@gmail.com>
2025-03-14 12:02:45 +0100

    Updated SOFTWARE_VERSION[]

--- opi_emac_artnet_dmx_multi/include/software_version.h
+++ opi_emac_artnet_dmx_multi/include/software_version.h
@@ -26,6 +26,6 @@
 #ifndef SOFTWARE_VERSION_H_
 #define SOFTWARE_VERSION_H_
 
-constexpr char SOFTWARE_VERSION[] = "4.5";
+constexpr char SOFTWARE_VERSION[] = "4.6";
 
 #endif /* SOFTWARE_VERSION_H_ */
--- opi_emac_artnet_pixel_multi/include/software_version.h
+++ opi_emac_artnet_pixel_multi/include/software_version.h
@@ -26,6 +26,6 @@
 #ifndef SOFTWARE_VERSION_H_
 #define SOFTWARE_VERSION_H_
 
-constexpr char SOFTWARE_VERSION[] = "3.8";
+constexpr char SOFTWARE_VERSION[] = "3.9";
 
 #endif /* SOFTWARE_VERSION_H_ */
