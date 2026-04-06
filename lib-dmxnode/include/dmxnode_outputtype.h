88616b1a8d33f4c5b81b19c0fb2aa0c61688cde6
Arjan <Arjan.van.Vught@gmail.com>
2026-04-06 16:58:03 +0200

    Remove DMX null output type
    Remove the obsolete OUTPUT_DMX_NULL implementation: delete dmxnodeoutputtypenull.h and remove all preprocessor branches that included or selected the null output type. This cleans up conditional includes and the DmxNodeOutputType/kOutputType mappings by eliminating the 'none'/null variant. Note: consumers that relied on OUTPUT_DMX_NULL will need to be updated to use a valid output type.

--- lib-dmxnode/include/dmxnode_outputtype.h
+++ lib-dmxnode/include/dmxnode_outputtype.h
@@ -138,10 +138,6 @@ using DmxPixelOutputType = PixelDmxMulti;
 #include "tlc59711dmx.h"
 #endif
 
-#if defined(OUTPUT_DMX_NULL)
-#include "dmxnodeoutputtypenull.h"
-#endif
-
 #if defined(DMXNODE_OUTPUT_PIXEL_DMX)
 #include "dmxnodewith4.h"
 using DmxNodeOutputType = DmxNodeWith4<CONFIG_DMXNODE_DMX_PORT_OFFSET>;
@@ -173,8 +169,6 @@ using DmxNodeOutputType = SparkFunDmx;
 using DmxNodeOutputType = PCA9685DmxSet;
 #elif defined(OUTPUT_DMX_TLC59711)
 using DmxNodeOutputType = TLC59711Dmx;
-#elif defined(OUTPUT_DMX_NULL)
-using DmxNodeOutputType = DmxNodeOutputTypeNull;
 #endif
 
 namespace dmxnode
@@ -187,8 +181,6 @@ inline constexpr auto kOutputType = OutputType::kDmx;
 inline constexpr auto kOutputType = OutputType::kPixel;
 #elif defined(DMXNODE_OUTPUT_PIXEL_DMX)
 inline constexpr auto kOutputType = OutputType::kPixelDmx;
-#elif defined(OUTPUT_DMX_NULL)
-inline constexpr auto kOutputType = OutputType::kNone;
 #else
 inline constexpr auto kOutputType = OutputType::kUndefined;
 #endif
