76ced72017a6eaad70aedc97a686ad52cfdf69c3
Arjan <Arjan.van.Vught@gmail.com>
2026-06-11 17:00:57 +0200

    Update copyright contact email
    Replace copyright header contact email from info@orangepi-dmx.nl to info@gd32-dmx.org across several source files. Affected files: lib-clib/src/strlen.cpp, lib-dmx/src/h3/multi/dmx.cpp, lib-flash/src/spi/h3/spi_flash.cpp, lib-h3/src/h3_cpu.cpp, lib-h3/src/h3_thermal.cpp, lib-h3/src/mac_address.cpp, and lib-widget/src/widget.cpp. No functional code changes — only header comment updates to reflect the new contact domain.

--- lib-clib/src/strlen.cpp
+++ lib-clib/src/strlen.cpp
@@ -2,7 +2,7 @@
  * @file strlen.cpp
  *
  */
-/* Copyright (C) 2023-2026 by Arjan van Vught mailto:info@orangepi-dmx.nl
+/* Copyright (C) 2023-2026 by Arjan van Vught mailto:info@gd32-dmx.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- lib-widget/src/widget.cpp
+++ lib-widget/src/widget.cpp
@@ -6,7 +6,7 @@
  * https://wiki.openlighting.org/index.php/USB_Protocol_Extensions
  *
  */
-/* Copyright (C) 2015-2026 by Arjan van Vught mailto:info@orangepi-dmx.nl
+/* Copyright (C) 2015-2026 by Arjan van Vught mailto:info@gd32-dmx.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
