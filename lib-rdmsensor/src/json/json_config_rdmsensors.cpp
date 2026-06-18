0bce410bab33b00248fa8f866f19c378869f7f66
Arjan <Arjan.van.Vught@gmail.com>
2026-06-18 15:54:47 +0200

    Reformat json_config_rdmsensors.cpp
    Update file formatting and copyright year. The copyright notice was extended to 2025-2026 and the namespace/function brace placement and indentation were reformatted. No functional or logic changes were made to GetRdmSensors or SetRdmSensors; only whitespace and style were adjusted.

--- lib-rdmsensor/src/json/json_config_rdmsensors.cpp
+++ lib-rdmsensor/src/json/json_config_rdmsensors.cpp
@@ -2,7 +2,7 @@
  * @file json_config_rdmsensors.cpp
  *
  */
-/* Copyright (C) 2025 by Arjan van Vught mailto:info@gd32-dmx.org
+/* Copyright (C) 2025-2026 by Arjan van Vught mailto:info@gd32-dmx.org
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,45 +31,38 @@
 #include "configstore.h"
 #include "common/utils/utils_hex.h"
 
-namespace json::config
-{
-uint32_t GetRdmSensors(char* buffer, uint32_t length)
-{
-	return json::helpers::Serialize(buffer, length, [&](JsonDoc& doc) {
-	    char a[32];
-	    for (size_t types = 0; types < json::RdmSensorsParams::KeysSize(); types++)
-	    {
-	        a[0] = '[';
-	        uint32_t k = 1;
-	        for (uint32_t j = 0; j < common::store::rdm::sensors::kMaxDevices; j++)
-	        {
-	            if ((k + 3) > 31) break;
-	            const auto kType = ConfigStore::Instance().RdmSensorsIndexedGetType(j);
-	            if (kType == types)
-	            {
-	                const auto kAddress = ConfigStore::Instance().RdmSensorsIndexedGetAddress(j);
-	                if (kAddress != 0)
-	                {
-	                    a[k++] = common::hex::ToCharLowercase(kAddress >> 4);
-	                    a[k++] = common::hex::ToCharLowercase(kAddress);
-	                    a[k++] = ',';
-	                }
-	            }
-	        }
-	
-	        if (a[k - 1] == ',') k -= 1;
-	
-	        a[k++] = ']';
-	        a[k] = '\0';
-	
-	        const auto& keys = json::RdmSensorsParams::Keys();
-	        doc[keys[types].GetName()] = a;
-	    }
+namespace json::config {
+uint32_t GetRdmSensors(char* buffer, uint32_t length) {
+    return json::helpers::Serialize(buffer, length, [&](JsonDoc& doc) {
+        char a[32];
+        for (size_t types = 0; types < json::RdmSensorsParams::KeysSize(); types++) {
+            a[0] = '[';
+            uint32_t k = 1;
+            for (uint32_t j = 0; j < common::store::rdm::sensors::kMaxDevices; j++) {
+                if ((k + 3) > 31) break;
+                const auto kType = ConfigStore::Instance().RdmSensorsIndexedGetType(j);
+                if (kType == types) {
+                    const auto kAddress = ConfigStore::Instance().RdmSensorsIndexedGetAddress(j);
+                    if (kAddress != 0) {
+                        a[k++] = common::hex::ToCharLowercase(kAddress >> 4);
+                        a[k++] = common::hex::ToCharLowercase(kAddress);
+                        a[k++] = ',';
+                    }
+                }
+            }
+
+            if (a[k - 1] == ',') k -= 1;
+
+            a[k++] = ']';
+            a[k] = '\0';
+
+            const auto& keys = json::RdmSensorsParams::Keys();
+            doc[keys[types].GetName()] = a;
+        }
     });
 }
 
-void SetRdmSensors(const char* buffer, uint32_t buffer_size)
-{
+void SetRdmSensors(const char* buffer, uint32_t buffer_size) {
     ::json::RdmSensorsParams rdmsensors_params;
     rdmsensors_params.Store(buffer, buffer_size);
 }
